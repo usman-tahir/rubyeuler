5a2a976edf2cadcc7909e2de326ccffe26fece39
kellyi <kinnes@gmail.com>
2014-01-11 17:02:49 -0500

    updated

--- euler3.rb
+++ euler3.rb
@@ -3,7 +3,7 @@ start_time = Time.new
 
 # probably cheating to use this?
 require 'prime'
-puts "Factorialize this! => "
+puts "Factorize this! => "
 q = gets.chomp.to_i
 z = q.prime_division
 puts z
