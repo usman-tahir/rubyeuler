eb60a1fa1a6b37678d46e8dcf349b174407f9ebc
kellyi <kinnes@gmail.com>
2014-01-11 20:51:32 -0500

    reduced to 4 digits

--- euler4.rb
+++ euler4.rb
@@ -2,20 +2,20 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 start_time = Time.new
-z = 999
-q = 999
-i = 999999
+z = 99
+q = 99
+i = 9999
 palin = Array.new()
 
-# all the palindromes from 999999 to 100001 stored with 999999 at index 0
-while i > 100001
-	f = i.to_s[0..2] 
-	g = i.to_s[3..-1]
+# All the palindromes from 999999 to 100001 get stored -- with 999999 at index 0.
+while i > 1001
+	f = i.to_s[0..1] 
+	g = i.to_s[2..-1]
 	g.reverse
 	f.to_i
 	g.to_i
 	if f = g
-		# puts "f is " + f + " and g is " + g # debug
+		# puts "f is " + f + " and g is " + g # For debugging.
 		h = f + g.reverse
 		h = h.to_i
 		palin.push h
@@ -24,6 +24,7 @@ while i > 100001
 		i = i - 1
 	end		
 end
-puts palin[0..20] # debug
+puts palin[0..20] # For debugging.
+
 
 printf("Run time %.4f s\n", Time.new - start_time)
