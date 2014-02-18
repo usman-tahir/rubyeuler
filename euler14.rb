4c575242b593a0cfb9900a52fd2bb23ab6d8c86b
kellyi <kinnes@gmail.com>
2014-02-17 22:05:34 -0500

    solved problem 14

---
+++ euler14.rb
@@ -0,0 +1,28 @@
+# http://projecteuler.net/problem=14
+runtime = Time.now
+
+sequence = Array.new
+store = Array.new
+
+for n in 2..999_999
+	sequence.push n
+	until n == 1
+		if n % 2 == 0
+			n = n / 2
+		else
+			n = (3 * n) + 1
+		end
+		sequence.push n
+	end
+	if sequence.count > store.count
+		store = sequence
+		sequence = []
+		puts store
+	else 
+		sequence = []	
+	end
+end	
+
+p store
+
+puts ((Time.now - runtime).to_f).to_s + " s"
