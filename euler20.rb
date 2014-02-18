ff2dfdaf445a7ddc2357e86c494fe8966abcba09
kellyi <kinnes@gmail.com>
2014-02-17 22:47:47 -0500

    solved problem 20

---
+++ euler20.rb
@@ -0,0 +1,15 @@
+# http://projecteuler.net/problem=20
+runtime = Time.now
+hundred = Array.new
+
+def factorial(num)
+	if num == 1
+		1
+	else
+		num * factorial(num-1)
+	end
+end
+
+hundred = factorial(100).to_s.split('').map(&:to_i)
+puts hundred.inject{|sum,x| sum + x}
+puts ((Time.now - runtime).to_f).to_s + " s"
