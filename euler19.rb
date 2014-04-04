c04f80cefdc3f044726e7f46ba88d80d72046862
kellyi <kinnes@gmail.com>
2014-04-04 17:17:41 -0400

    solved problem 19

--- euler19.rb
+++ euler19.rb
@@ -4,12 +4,13 @@ runtime = Time.now
 def sundays
 	sundays = Array.new
 	t = Time.gm(1901,"jan",1)
+	day = 86400
 	until t.strftime("%D") == "12/31/00"
 		if t.sunday? == true
 			sundays.push t.strftime("%d")
 		else
 		end
-		t = t + 86400
+		t = t + day
 	end
 	puts sundays.count("01").to_s + " Sundays fell on the first of the Month from 1/1/1901 to 12/31/00."
 end
