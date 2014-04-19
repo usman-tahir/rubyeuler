0a018266a5629df193050076867c396ad81bcbfa
kellyi <kinnes@gmail.com>
2014-04-18 23:29:04 -0400

    cleaning up

--- euler32.rb
+++ euler32.rb
@@ -6,8 +6,8 @@ for x in 1..5000
   for y in 1..987
     z = x * y
     if (x.to_s.scan(/./).map {|e| e.to_i} + y.to_s.scan(/./).map {|e| e.to_i} + z.to_s.scan(/./).map {|e| e.to_i}).sort == [1,2,3,4,5,6,7,8,9]
-    puts x.to_s + " * " + y.to_s + " = " + z.to_s
-    accumulator.push z
+      puts x.to_s + " * " + y.to_s + " = " + z.to_s
+      accumulator.push z
     else
     end
   y += 1	
