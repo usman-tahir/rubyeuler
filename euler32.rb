7761786391a7e61e174f19dedcdfb552ca26ba13
kellyi <kinnes@gmail.com>
2014-04-18 23:28:14 -0400

    cleaning up

--- euler32.rb
+++ euler32.rb
@@ -2,7 +2,7 @@
 runtime = Time.now
 
 accumulator = []
-for x in 1..2345
+for x in 1..5000
   for y in 1..987
     z = x * y
     if (x.to_s.scan(/./).map {|e| e.to_i} + y.to_s.scan(/./).map {|e| e.to_i} + z.to_s.scan(/./).map {|e| e.to_i}).sort == [1,2,3,4,5,6,7,8,9]
