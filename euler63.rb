5f41f57556de1b02c2fe82da89a4688b4f8e47ef
kellyi <kinnes@gmail.com>
2014-05-04 00:01:33 -0400

    solved problem 63

--- euler63.rb
+++ euler63.rb
@@ -1,10 +1,10 @@
 starttime = Time.now
 
 npowerints =[]
-(1..100).each do |power|
-	(1..9).each do |base|
-		npowerints.push (base**power) if (base**power).to_s.length == power
-	end
+(1..100).each do |power| # I tested several maximums until it stopped returning new npowerints.
+  (1..9).each do |base|
+    npowerints.push (base**power) if (base**power).to_s.length == power
+  end
 end		
 
 puts npowerints.count
