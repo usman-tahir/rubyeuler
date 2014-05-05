cfa7f4008d27959812db5b3d4f2073eb50b4066b
kellyi <kinnes@gmail.com>
2014-05-04 23:31:35 -0400

    solved problem 92 (inefficiently)

--- euler92.rb
+++ euler92.rb
@@ -2,22 +2,22 @@
 starttime = Time.now
 
 def square_digit_chain(number)
-	chainnumber = number
-	while chainnumber != 1 && chainnumber != 89
-		testarray =[]
-		chainnumber.to_s.split('').map(&:to_i).each do |digit|
-			testarray.push (digit ** 2)
-		end
-		chainnumber = testarray.inject(:+)
-	  return 1 if chainnumber == 1
-	  return 89 if chainnumber == 89
-	end   			
+  chainnumber = number
+  while chainnumber != 1 && chainnumber != 89
+    testarray =[]
+    chainnumber.to_s.split('').map(&:to_i).each do |digit|
+    testarray.push (digit ** 2)
+  end
+  chainnumber = testarray.inject(:+)
+  return 1 if chainnumber == 1
+  return 89 if chainnumber == 89
+  end   			
 end
 
 eightynines =[]
 (1..10_000_000).each do |number|
-	puts number
-	eightynines.push number if square_digit_chain(number) == 89
+  puts number
+  eightynines.push number if square_digit_chain(number) == 89
 end
 
 puts eightynines.count	
