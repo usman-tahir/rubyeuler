115203b3da05115ecc4cc7d70b06aba163b4472e
kellyi <kinnes@gmail.com>
2014-06-02 20:41:05 -0400

    rosetta code guess the number

--- guessnumber.rb
+++ guessnumber.rb
@@ -5,11 +5,11 @@ random_number = rand(1..10)
 guess = 0
 
 until guess == random_number
-	print "Guess a number from 1 to 10: "
-	guess = gets.to_i
-	if guess == random_number
-		puts "Well guessed! It's " + random_number.to_s
-	else
-	  puts "Guess again!"
-	end
+  print "Guess a number from 1 to 10: "
+  guess = gets.to_i
+  if guess == random_number
+    puts "Well guessed! It's " + random_number.to_s
+  else
+    puts "Guess again!"
+  end
 end	  	
