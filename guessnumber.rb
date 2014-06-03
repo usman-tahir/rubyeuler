9ea28c9c3c3f5739f5d65d12f673a0cfc0977ca7
kellyi <kinnes@gmail.com>
2014-06-02 20:40:02 -0400

    guess the number rosettacode

---
+++ guessnumber.rb
@@ -0,0 +1,15 @@
+# http://rosettacode.org/wiki/Guess_the_number
+
+random_number = rand(1..10)
+
+guess = 0
+
+until guess == random_number
+	print "Guess a number from 1 to 10: "
+	guess = gets.to_i
+	if guess == random_number
+		puts "Well guessed! It's " + random_number.to_s
+	else
+	  puts "Guess again!"
+	end
+end	  	
