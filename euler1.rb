11a6beb3c4158188aa439d7a2c665bf994d79dea
kellyi <kinnes@gmail.com>
2014-06-09 17:41:05 -0400

    dropped array from solution 1

--- euler1.rb
+++ euler1.rb
@@ -1,10 +1,10 @@
 # http://projecteuler.net/problem=1
 start_time = Time.now
 
-array_of_multiples =[]
+accumulator = 0
 (1..999).each do |number|
-	array_of_multiples.push number if number % 3 == 0 || number % 5 == 0
+  accumulator += number if number % 3 == 0 || number % 5 == 0
 end
 
-puts array_of_multiples.inject(:+)	
+puts accumulator
 puts ((Time.now - start_time).to_f).to_s + "s"
