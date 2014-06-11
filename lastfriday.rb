363bb518faa08e54f104dc0ed61ad9b4d55fcd58
kellyi <kinnes@gmail.com>
2014-06-11 02:01:00 -0400

    find last fridays of the month for a given year

---
+++ lastfriday.rb
@@ -0,0 +1,23 @@
+# http://rosettacode.org/wiki/Last_Friday_of_each_month
+start_time = Time.now
+
+def find_last_friday(year)
+  fridays = []
+  time_counter = Time.new(year)
+  until time_counter == Time.new(year+1)
+    fridays << time_counter if time_counter.friday? == true
+    time_counter += (60 * 60 * 24)
+  end
+  last_fridays =[]
+  fridays.each_index do |index|
+    break if fridays[index+1] == nil
+    if fridays[index+1].month > fridays[index].month
+      last_fridays << fridays[index]
+    end
+  end
+  last_fridays << fridays[-1] # last Fri of the year
+  puts last_fridays
+end
+
+find_last_friday(2012)
+puts ((Time.now - start_time).to_f).to_s + "s"
