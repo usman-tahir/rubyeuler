4499ca96b1531c9429363209f3ca00e28795a226
kellyi <kinnes@gmail.com>
2014-06-13 01:17:49 -0400

    find best shuffle of a string

---
+++ bestshuffle.rb
@@ -0,0 +1,26 @@
+# http://rosettacode.org/wiki/Best_shuffle
+start_time = Time.now
+
+strings_to_test = ["abracadabra", "seesaw", "elk", "grrrrrr", "up", "a"]
+
+def find_best_shuffle(string)
+  array_to_shuffle = string.scan(/./).to_a
+  best_shuffle = false
+  until best_shuffle == true
+    shuffled_array = array_to_shuffle.shuffle
+    similarity_counter = 0
+    shuffled_array.each_index do |index|
+      similarity_counter += 1 if shuffled_array[index] == array_to_shuffle[index]
+    end
+    best_shuffle = true if similarity_counter == 0
+    best_shuffle = true if array_to_shuffle.count < 2
+    best_shuffle = true if array_to_shuffle.count > array_to_shuffle.uniq.count * 3 && similarity_counter < array_to_shuffle.count
+  end
+  return string + ", " + shuffled_array.join + ", (" + similarity_counter.to_s + ")"
+end
+
+strings_to_test.each do |test_string|
+  puts find_best_shuffle(test_string)
+end
+
+puts ((Time.now - start_time).to_f).to_s + "s"	
