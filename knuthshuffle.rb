2821c7cb05039f6daf58a7f681c7e609ee1f815f
kellyi <kinnes@gmail.com>
2014-06-21 21:36:28 -0400

    knuth shuffle implementation

---
+++ knuthshuffle.rb
@@ -0,0 +1,14 @@
+# http://rosettacode.org/wiki/Knuth_shuffle
+start_time = Time.now
+test_array = [0,1,2,3,4,5,6,7,8,9]
+
+def knuth_shuffle(array_to_shuffle)
+  array_to_shuffle.each_index do |index|
+    random_index = rand(array_to_shuffle.count-1)
+    array_to_shuffle[index], array_to_shuffle[random_index] = array_to_shuffle[random_index], array_to_shuffle[index]
+  end
+  array_to_shuffle
+end  	
+
+puts knuth_shuffle(test_array).inspect
+puts ((Time.now - start_time).to_f).to_s + "s"
