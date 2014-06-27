fab1a00670e793bc90995a59c3052ddc20f50c82
kellyi <kinnes@gmail.com>
2014-06-26 23:10:59 -0400

    add ordinal suffixes

---
+++ nth.rb
@@ -0,0 +1,38 @@
+# http://rosettacode.org/wiki/N%27th
+start_time = Time.now
+
+def nth(number)
+  postfix = "th"
+  number = number.to_s
+  number.length
+  if number.length == 1
+    case
+    when number[-1] == "1" then postfix = "st"
+    when number[-1] == "2" then postfix = "nd"
+    when number[-1] == "3" then postfix = "rd"
+    end
+  elsif number.length > 1 && number[-2] != "1"
+    case
+    when number[-1] == "1" then postfix = "st"
+    when number[-1] == "2" then postfix = "nd"
+    when number[-1] == "3" then postfix = "rd"
+    end
+  else
+    postfix = "th"
+  end
+  number << postfix	
+end
+
+def find_nth(start_of_range, end_of_range)
+  nths =[]
+  (start_of_range..end_of_range).each do |number|
+    nths << nth(number)
+  end
+  nths
+end		
+
+puts find_nth(0,25).inspect
+puts find_nth(250,265).inspect
+puts find_nth(1000,1025).inspect
+
+puts ((Time.now-start_time).to_f).to_s + "s"
