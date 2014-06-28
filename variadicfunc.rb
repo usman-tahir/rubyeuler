7feea0ad1033e40276fafdef4661c5b0b52a3d9f
kellyi <kinnes@gmail.com>
2014-06-28 11:14:08 -0400

    cut some redundancy

--- variadicfunc.rb
+++ variadicfunc.rb
@@ -4,23 +4,21 @@ start_time = Time.now
 def cardinal_to_ordinal(*numbers)
   ordinals = []
   numbers.each do |number|
-  postfix = "th"
-  number = number.to_s
-  number.length
-  if number[-2] != "1"
-    case
-    when number[-1] == "1" then postfix = "st"
-    when number[-1] == "2" then postfix = "nd"
-    when number[-1] == "3" then postfix = "rd"
+    postfix = "th"
+    number = number.to_s
+    if number[-2] != "1"
+      case
+      when number[-1] == "1" then postfix = "st"
+      when number[-1] == "2" then postfix = "nd"
+      when number[-1] == "3" then postfix = "rd"
+      end
     end
-  end
-  number << postfix
-  ordinals << number
+    ordinals << (number << postfix)
   end
   ordinals
 end
 
-puts cardinal_to_ordinal(1,11,21,34,114)
+puts cardinal_to_ordinal(1,11,21,34,114,200_112,1_000_023)
 
 puts ((Time.now - start_time).to_f).to_s + "s"    	
 
