84ee3af4abac451169e6e9ee217758a7211611bd
kellyi <kinnes@gmail.com>
2014-07-07 21:47:46 -0400

    print one through five on one line; six through ten on the next

---
+++ loop_continue.rb
@@ -0,0 +1,10 @@
+# http://rosettacode.org/wiki/Loops/Continue
+start_time = Time.now
+
+(1..10).each do |number|
+  print number.to_s + ", " if number % 5 != 0 && number % 10 != 0
+  puts number.to_s + ", " if number % 5 == 0 && number/5 == 1
+  print number.to_s + "\n" if number % 10 == 0
+end  
+
+puts ((Time.now-start_time).to_f).to_s + "s"
