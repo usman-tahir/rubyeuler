8dce4dceb54b409a363c4b6e9e0c3f5643a632b5
kellyi <kinnes@gmail.com>
2014-07-18 17:37:21 -0400

    bogosort using a singleton method

--- bogosort.rb
+++ bogosort.rb
@@ -2,17 +2,20 @@
 start_time = Time.now
 array_to_sort1 = [26, 45, 4, 98, 769, 234, 24324, 78, 865, 0, 6, 123, 7, 14345, 5, 12, 211, 1, 3215, 67, 8, 90, 9, 2345, 456]
 array_to_sort2 = [7,3,1,5,10,0,4]
+
 test_array = array_to_sort2
+def test_array.bogosort
+  bogosorted_array = self
+  sorted_test_array = bogosorted_array.sort # to check sorting
+  counter = 0
+  until bogosorted_array = sorted_test_array
+    bogosorted_array.shuffle!
+    puts (counter += 1).to_s + "attempts" if counter > 1
+  end	
+  bogosorted_array
+end	
 
-bogosorted_array = test_array
-sorted_test_array = test_array.sort
-counter = 0
-until sorted_test_array == bogosorted_array
-  bogosorted_array.shuffle!
-  puts counter += 1
-end
-
-puts sorted_test_array == bogosorted_array
-puts "It took #{counter} bogosort attempts."  
+puts test_array.bogosort.inspect
+puts test_array.bogosort == test_array.sort 
 
 puts ((Time.now - start_time).to_f).to_s + "s"
