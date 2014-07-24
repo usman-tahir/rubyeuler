c221b4233629aa46eeaef6cfebfdccb6c4275ba2
kellyi <kinnes@gmail.com>
2014-07-24 17:16:49 -0400

    heredoc with defined endline

--- heredoc.rb
+++ heredoc.rb
@@ -1,12 +1,20 @@
 # http://rosettacode.org/wiki/Here_document
 start_time = Time.now
 document = <<END
-  Once upon a time
-  There was a string
-  That was enclosed differently
-  Than other strings.
+Once upon a time
+There was a string
+That was enclosed differently
+Than other strings.
 END
 
+document2 = <<'THIS'
+If you've got a long string,
+Like one that goes across multiple
+Paragraphs, you can use a heredoc
+Instead of a regular string.
+THIS
+
 puts document
+puts document2
 puts ((Time.now-start_time).to_f).to_s + "s"
 
