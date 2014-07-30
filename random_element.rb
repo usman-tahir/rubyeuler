d3c4cf416586a12fdd778e85931d88a5d8b5b79f
Kelly Innes <kinnes@gmail.com>
2014-07-30 19:10:23 -0400

    choose a random element

---
+++ random_element.rb
@@ -0,0 +1,9 @@
+# http://rosettacode.org/wiki/Pick_random_element
+# 
+list_of_months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+def list_of_months.select_random_element
+  element = self.sample
+end
+
+puts list_of_months.select_random_element
