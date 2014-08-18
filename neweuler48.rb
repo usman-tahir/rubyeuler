da783f61854f3e26c9729f842e38d0ec6aa5fb49
Kelly Innes <kinnes@gmail.com>
2014-08-18 00:27:27 -0400

    solved problem 48 again in ruby

---
+++ neweuler48.rb
@@ -0,0 +1,11 @@
+# http://projecteuler.net/problem=48
+
+def self_power_sum_up_to(number)
+  sum_total = 0
+  (1..number).each do |num|
+    sum_total += num**num
+  end
+  sum_total
+end
+
+puts self_power_sum_up_to(1000).to_s[-10..-1]    
