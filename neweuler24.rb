6ede8f76b2f90808898af19c427faea3e735455f
Kelly Innes <kinnes@gmail.com>
2014-08-18 21:38:35 -0400

    solved problem 24 again in ruby

---
+++ neweuler24.rb
@@ -0,0 +1,9 @@
+# http://projecteuler.net/problem=24
+pandigital_array = [0,1,2,3,4,5,6,7,8,9]
+
+def pandigital_array.millionth_permutation
+  permutations_array = self.permutation.to_a
+  permutations_array[999_999].join
+end
+
+puts pandigital_array.millionth_permutation  
