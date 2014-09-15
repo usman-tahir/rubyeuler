3ad645e893474b08248b71e64b8cc3641575abd6
Kelly Innes <kinnes@gmail.com>
2014-09-14 23:26:47 -0400

    refined variable names

--- seive_of_eratosthones.rb
+++ seive_of_eratosthones.rb
@@ -1,15 +1,15 @@
 # http://rosettacode.org/wiki/Sieve_of_Eratosthenes
 
-def find_primes_upto(number)
-  primes = (2..number).to_a
-  counter = 0
-  until counter >= primes.count  
+def find_primes_upto(int)
+  primes = (2..int).to_a
+  index_counter = 0
+  until index_counter >= primes.count  
     primes.each do |number|
-      if number != primes[counter]
-        primes.delete(number) if number % primes[counter] == 0
+      if number != primes[index_counter]
+        primes.delete(number) if number % primes[index_counter] == 0
       end  
     end
-    counter += 1
+    index_counter += 1
   end          
   primes
 end
