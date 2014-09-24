3d0c1c03fc9ac876dd88870bf33c61602fcea7ef
Kelly Innes <kinnes@gmail.com>
2014-09-23 21:24:15 -0400

    some recursive arithmetic functions in scheme

---
+++ recursive_arithmetic.scm
@@ -0,0 +1,24 @@
+(define (iszero x)
+  (= 0 x))
+
+(define (succ x)
+  (+ x 1))
+
+(define (pred x)
+  (- x 1))
+
+(define (add x y)
+  (if (iszero y)
+      (eval x)
+      (add (succ x) (pred y))))
+
+(define (subtract x y)
+  (if (iszero y)
+    (eval x)
+    (subtract (pred x) (pred y))))
+
+(define (multiply x y)
+  (cond ((iszero y) (eval y))
+        ((iszero (pred y)) (eval x))
+        (else (add (multiply (eval x) (pred y)) x))))
+
