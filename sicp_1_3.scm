9de70ca29083b896d0266a15ee74bdd0e36c4d29
Kelly Innes <kinnes@gmail.com>
2014-09-24 23:52:42 -0400

    cleaning up

--- sicp_1_3.scm
+++ sicp_1_3.scm
@@ -9,7 +9,7 @@
 (define (find-max x y z)
   (cond ((and (> x y) (> x z)) x)
         ((and (> y x) (> y z)) y)
-        ((and (> z x) (> z y)) z)
+        ((and (> z x) (> z y)) z)))
 
 (define (find-mid x y z)
   (cond ((and (= (find-max x y z) x) (> y z)) y)
