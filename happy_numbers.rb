11834032122fd8e38da708dda6ebe6894696bdcc
Kelly Innes <kinnes@gmail.com>
2014-10-01 23:47:03 -0400

    cleaning up methods

--- happy_numbers.rb
+++ happy_numbers.rb
@@ -1,31 +1,29 @@
 # http://rosettacode.org/wiki/Happy_numbers
+# http://en.wikipedia.org/wiki/Happy_number
 load "eulermethods.rb"
 
 def sum_square_digits(number)
   number.to_a.map { |digit| digit ** 2 }.inject(:+)
 end
 
-def happy_or_unhappy?(number)
+def happy?(number)
   chain_array = [number]
   temp_value = number
   until chain_array[-1] == 89 || chain_array[-1] == 1
-    chain_array << sum_square_digits(temp_value)
     temp_value = sum_square_digits(temp_value)
+    chain_array << temp_value
   end
-  "unhappy" if chain_array[-1] == 89
-  "happy" if chain_array[-1] == 1
+  chain_array[-1] == 1
 end
 
 def find_happy_numbers(number)
-  happy_numbers_array =[]
+  happy_numbers =[]
   counter = 1
-  until happy_numbers_array.count == number
-    if happy_or_unhappy?(counter) == "happy"
-      happy_numbers_array << counter
-    end
+  until happy_numbers.count == number
+    happy_numbers << counter if happy?(counter)
     counter += 1  
   end
-  happy_numbers_array  
+  happy_numbers
 end  
 
-p find_happy_numbers(8)
+p find_happy_numbers(143)
