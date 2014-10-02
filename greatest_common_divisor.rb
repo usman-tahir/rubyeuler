61b3f73a83bbe3cd57ea0159cf3b4aac9f4e6f61
Kelly Innes <kinnes@gmail.com>
2014-10-01 22:56:47 -0400

    use existing lcm and gcd method

---
+++ greatest_common_divisor.rb
@@ -0,0 +1,7 @@
+# http://rosettacode.org/wiki/Greatest_common_divisor
+
+def find_gcd(first_num,second_num)
+  first_num.gcd(second_num)
+end
+
+p find_gcd(48,180)  
---
+++ least_common_multiple.rb
@@ -0,0 +1,7 @@
+# http://rosettacode.org/wiki/Least_common_multiple
+
+def find_lcm(first_num,second_num)
+  first_num.lcm(second_num)
+end
+
+p find_lcm(12,18)
