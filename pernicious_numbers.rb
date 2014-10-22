0eb388a6e47ae90a93dec91416e9477308b76335
Kelly Innes <kinnes@gmail.com>
2014-10-22 18:15:18 -0400

    streamlined methods and corrected typo

--- pernicious_numbers.rb
+++ pernicious_numbers.rb
@@ -1,19 +1,15 @@
 # http://rosettacode.org/wiki/Pernicious_numbers
 require 'Prime'
 
-def number_to_binary_string(number)
-  number.to_s(2)
-end
-
-def pernicious(binary_string)
-  binary_string.split(//).to_a.count("1").prime?
+def pernicious(number)
+  number.to_s(2).split(//).to_a.count("1").prime?
 end
 
 def find_first_25_pernicious_numbers
   pernicious_numbers = []    
   counter = 1
   until pernicious_numbers.count == 25
-    if pernicious(number_to_binary_string(counter))
+    if pernicious(counter)
       pernicious_numbers << counter
     else
     end
@@ -25,10 +21,10 @@ end
 def find_888s
   pernicious_numbers = []
   (888_888_877..888_888_888).each do |number|
-    pernicious_numbers << number if pernicious(number_to_binary_string(number))
+    pernicious_numbers << number if pernicious(number)
   end
   pernicious_numbers
 end
 
 puts "First 25 pernicious numbers: #{find_first_25_pernicious_numbers.inspect}"     
-puts "Pernicious numbers from ..877 to ..888 from #{find_888s.inspect}"
+puts "Pernicious numbers from ..877 to ..888 are: #{find_888s.inspect}"
