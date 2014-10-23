98fb9c6b7e6078332790a074b0a93bd1ea64c545
Kelly Innes <kinnes@gmail.com>
2014-10-23 19:20:49 -0400

    find ordered words in the unix dictionary

--- fortune.rb
+++ fortune.rb
@@ -1,9 +1,7 @@
 # http://programmingpraxis.com/2011/04/05/fortune/
 # http://minnie.tuhs.org/cgi-bin/utree.pl?file=V7/usr/games/lib/fortunes
-start_time = Time.now
 
 fortunes =[]
 File.open('fortunes.txt').each_line { |line| fortunes << line }
 puts fortunes.sample.to_s
 
-puts ((Time.now - start_time).to_f).to_s + "s"  
---
+++ ordered_words.rb
@@ -0,0 +1,23 @@
+# http://rosettacode.org/wiki/Ordered_words
+# http://www.puzzlers.org/pub/wordlists/unixdict.txt
+
+def ordered_word(string)
+  letters = string.downcase.split(//).to_a
+  limit = (letters.count - 2)
+  (0..limit).each do |index|
+    return false if letters[index] > letters[index+1]
+  end
+  return true
+end
+
+def find_ordered_words_in_dictionary
+  ordered_words = []
+  words_to_test = File.open("unixdict.txt").readlines
+  words_to_test.map {|word| word.chomp!}
+  words_to_test.each do |word|
+    ordered_words << word if ordered_word(word)
+  end
+  ordered_words
+end
+
+puts find_ordered_words_in_dictionary
