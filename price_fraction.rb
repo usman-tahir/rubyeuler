5b2917c18ce026149b6415e1f2c0cb9d260a6971
kellyi <kinnes@gmail.com>
2014-10-24 11:51:12 -0400

    changed indentation

--- price_fraction.rb
+++ price_fraction.rb
@@ -1,50 +1,50 @@
 # http://rosettacode.org/wiki/Price_fraction
 
 def price_fraction(floating_pt_nm)
-	case
-	when floating_pt_nm >= 0.00 && floating_pt_nm < 0.06
-		0.10
+  case
+  when floating_pt_nm >= 0.00 && floating_pt_nm < 0.06
+    0.10
   when floating_pt_nm >= 0.06 && floating_pt_nm < 0.11
-  	0.18
+    0.18
   when floating_pt_nm >= 0.11 && floating_pt_nm < 0.16
     0.26
   when floating_pt_nm >=  0.16 && floating_pt_nm <  0.21  
-  	0.32
-	when floating_pt_nm >=  0.21  && floating_pt_nm <  0.26 
-		0.38
+    0.32
+  when floating_pt_nm >=  0.21  && floating_pt_nm <  0.26 
+    0.38
   when floating_pt_nm >=  0.26  && floating_pt_nm <  0.31  
-  	0.44
-	when floating_pt_nm >=  0.31  && floating_pt_nm <  0.36  
-		0.50
-	when floating_pt_nm >=  0.36  && floating_pt_nm <  0.41  
-		0.54
-	when floating_pt_nm >=  0.41  && floating_pt_nm <  0.46  
-		0.58
-	when floating_pt_nm >=  0.46  && floating_pt_nm <  0.51  
-		0.62
-	when floating_pt_nm >=  0.51  && floating_pt_nm <  0.56  
-		0.66
-	when floating_pt_nm >=  0.56  && floating_pt_nm <  0.61  
-		0.70
-	when floating_pt_nm >=  0.61  && floating_pt_nm <  0.66  
-		0.74
-	when floating_pt_nm >=  0.66  && floating_pt_nm <  0.71  
-		0.78
-	when floating_pt_nm >=  0.71  && floating_pt_nm <  0.76  
-		0.82
-	when floating_pt_nm >=  0.76  && floating_pt_nm <  0.81  
-		0.86
-	when floating_pt_nm >=  0.81  && floating_pt_nm <  0.86  
-		0.90
-	when floating_pt_nm >=  0.86  && floating_pt_nm <  0.91  
-		0.94
-	when floating_pt_nm >=  0.91  && floating_pt_nm <  0.96  
-		0.98
-	when floating_pt_nm >=  0.96  && floating_pt_nm <  1.01  
-		1.00
-	else
-	  raise "Something went wrong!" 
-	end
+    0.44
+  when floating_pt_nm >=  0.31  && floating_pt_nm <  0.36  
+    0.50
+  when floating_pt_nm >=  0.36  && floating_pt_nm <  0.41  
+    0.54
+  when floating_pt_nm >=  0.41  && floating_pt_nm <  0.46  
+    0.58
+  when floating_pt_nm >=  0.46  && floating_pt_nm <  0.51  
+    0.62
+  when floating_pt_nm >=  0.51  && floating_pt_nm <  0.56  
+    0.66
+  when floating_pt_nm >=  0.56  && floating_pt_nm <  0.61  
+    0.70
+  when floating_pt_nm >=  0.61  && floating_pt_nm <  0.66  
+    0.74
+  when floating_pt_nm >=  0.66  && floating_pt_nm <  0.71  
+    0.78
+  when floating_pt_nm >=  0.71  && floating_pt_nm <  0.76  
+    0.82
+  when floating_pt_nm >=  0.76  && floating_pt_nm <  0.81  
+    0.86
+  when floating_pt_nm >=  0.81  && floating_pt_nm <  0.86  
+    0.90
+  when floating_pt_nm >=  0.86  && floating_pt_nm <  0.91  
+    0.94
+  when floating_pt_nm >=  0.91  && floating_pt_nm <  0.96  
+    0.98
+  when floating_pt_nm >=  0.96  && floating_pt_nm <  1.01  
+    1.00
+  else
+    raise "Something went wrong!" 
+  end
 end
 
 def test_values
@@ -56,8 +56,8 @@ def test_values
 end
 
 test_values.each do |value|
-	puts "#{value} -> #{price_fraction(value).to_f}"
-end	
+  puts "#{value} -> #{price_fraction(value).to_f}"
+end 
 
 
 
