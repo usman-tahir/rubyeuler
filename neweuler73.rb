b0297a69d16310547f37da36f8b0837c712277b8
Kelly Innes <kinnes@gmail.com>
2014-10-29 21:05:08 -0400

    solved problem 73 in ruby

---
+++ neweuler73.rb
@@ -0,0 +1,17 @@
+# https://projecteuler.net/problem=73
+
+def reduced_fractions_in_range(max_denominator)
+  fractions = []
+  (1..max_denominator).each do |denominator|
+    (1...denominator).each do |numerator|
+      fraction = numerator/denominator.to_r
+      if fraction > 1/3.to_r && fraction < 1/2.to_r
+        puts fraction
+        fractions << fraction
+      end  
+    end
+  end
+  fractions.uniq
+end
+
+puts reduced_fractions_in_range(12_000).count      
