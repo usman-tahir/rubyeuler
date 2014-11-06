d930cfe40062dbf44d411dcfafbdeb4a338a3f8b
Kelly Innes <kinnes@gmail.com>
2014-11-05 20:07:02 -0500

    given a url, find its ip address

--- dns_query.rb
+++ dns_query.rb
@@ -2,10 +2,11 @@
 require 'socket'
 
 def find_ip(url)
-  Socket.getaddrinfo(url, "http", nil, :STREAM)[0][3]
+  Addrinfo.getaddrinfo(url, nil, nil, :DGRAM).map! {|ai| ai.ip_address}
 end
 
 p find_ip("www.ruby-lang.org")
 p find_ip("www.google.com")
 p find_ip("yahoo.com")
-p find_ip("twitter.com")  
+p find_ip("twitter.com") 
+p find_ip("www.kame.net") 
