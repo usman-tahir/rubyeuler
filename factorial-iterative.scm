06dff6be491314438d9be8de3c056a859aeade83
kellyi <kinnes@gmail.com>
2014-11-10 18:53:28 -0500

    changed argument name

--- factorial-iterative.scm
+++ factorial-iterative.scm
@@ -1,9 +1,9 @@
 (define (factorial number)
   (factorial-iterator 1 1 number))
 
-(define (factorial-iterator product accumulator max-count)
-  (if (> accumulator max-count)
+(define (factorial-iterator product counter max-count)
+  (if (> counter max-count)
       product
-      (factorial-iterator (* accumulator product)
-			  (+ accumulator 1)
+      (factorial-iterator (* counter product)
+			  (+ counter 1)
 			  max-count)))
--- factorial_iterative.rb
+++ factorial_iterative.rb
@@ -2,11 +2,11 @@ def factorial(number)
   factorial_iterative(1,1,number)
 end
 
-def factorial_iterative(product,accumulator,max_count)
-  if accumulator > max_count
+def factorial_iterative(product,counter,max_count)
+  if counter > max_count
     return product
   else
-    factorial_iterative((product*accumulator),(accumulator+1),(max_count))
+    factorial_iterative((product*counter),(counter+1),(max_count))
   end
 end
 
