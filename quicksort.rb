d41c0b8b7736466e0de161f93a7670f6b38681fb
kellyi <kinnes@gmail.com>
2014-11-12 18:06:01 -0500

    quicksort implementation in ruby

---
+++ quicksort.rb
@@ -0,0 +1,22 @@
+# http://rosettacode.org/wiki/Sorting_algorithms/Quicksort
+
+def quicksort(array)
+  return array if array.length <= 1
+  lesser = []; equiv = []; greater = []
+  pivot = array[array.length/2]
+  array.each do |element|
+    if element < pivot
+      lesser << element
+    elsif element > pivot
+      greater << element
+    else element == pivot
+      equiv << element     
+    end
+  end   
+  quicksort(lesser) + equiv + quicksort(greater) 
+end    
+         
+
+
+test_array = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1]
+p quicksort(test_array)
