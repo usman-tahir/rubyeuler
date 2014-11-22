5325fe471f477567c5a23d29500b1caafa174dd8
Kelly Innes <kinnes@gmail.com>
2014-11-22 17:44:05 -0500

    move all nonzero int to left end of an array and all zero int to right end

--- array_of_zeroes.rb
+++ array_of_zeroes.rb
@@ -1,12 +1,5 @@
 # http://programmingpraxis.com/2014/11/21/an-array-of-zeroes/
 
-# You are given an array of integers. Write a program that moves all non-zero 
-# integers to the left end of the array, and all zeroes to the right end of 
-# the array. Your program should operate in place. The order of the non-zero 
-# integers doesn’t matter. As an example, given the input array 
-# [1,0,2,0,0,3,4], your program should permute the array to [1,4,2,3,0,0,0] 
-# or something similar, and return the value 4.
-
 def move_zeroes(array)
   right = (array.length - 1)
   left = 0
