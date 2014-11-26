181cf5bb68d048db450d771c85ab9c467d4e27f6
kellyi <kinnes@gmail.com>
2014-11-26 16:00:56 -0500

    generate json from a hash in ruby

--- json.rb
+++ json.rb
@@ -1,4 +1,4 @@
 require 'json'
 
-test_hash = {:hello => "goodbye"}
-puts JSON.generate(test_hash) => "{\"hello\":\"goodbye\"}"
+test_hash = Hash[("a".."z").to_a.zip((1..26).to_a)]
+puts JSON.generate(test_hash)
