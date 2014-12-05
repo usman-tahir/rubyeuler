eaa25804bdb60776c1fafdd94dbe7fe2a61a12eb
Kelly Innes <kinnes@gmail.com>
2014-12-05 00:52:04 -0500

    partially working ruby rewrite of a recursive scheme regex parser

---
+++ regex_matcher2.rb
@@ -0,0 +1,97 @@
+# http://programmingpraxis.com/2009/09/11/beautiful-code/
+# warning: hacky, non-beautiful version!
+require 'minitest/autorun'
+
+# match literal chars
+# match dot for any char
+# * for 0 or more of a char
+# ^ to begin string
+# $ to end string
+# match: search for re anywhere in text
+
+def trex(regex,text)
+  match(regex.split(//),text.split(//))
+end
+
+def match(regex,text)
+  p "called match(#{regex},#{text})"
+  case
+  when regex == nil
+    true
+  when text == nil
+    false  
+  when regex[0] == '^'
+    match_here(regex[1..-1],text)
+  else 
+    match_here(regex,text) || match(regex[1..-1],text[1..-1])
+  end  
+end
+
+def match_here(regex,text)
+  p "called match_here(#{regex},#{text})"
+  case
+  when regex == nil
+    true
+  when regex.count >= 2 && regex[1] == '*'
+    match_star(regex[0],regex[1..-1],text)
+  when regex[0] == '$' && regex[1] == nil
+    text == nil
+  when text.count >= 2 && (regex[0] == '.' || regex[0] == text[0])
+    match_here(regex[1..-1],text[1..-1])
+  else
+    false
+  end          
+end
+
+def match_star(c,regex,text)
+  p "called match_star(#{c},#{regex},#{text})"
+  case
+  when match_here(regex,text)
+    true
+  when text.count >= 2 && (text[0] == c || c == '.')
+    match_star(c,regex,text[1..-1])
+  else
+    false
+  end
+end        
+
+class TestMatch < MiniTest::Unit::TestCase
+
+  def test_match
+    assert trex('foo','foo')
+  end
+
+  def test_carrot
+    assert trex('^f.o','food') 
+  end
+
+  def test_star
+    assert trex('fo*','food')
+  end  
+  
+  def test_dollar
+    assert trex('ood$','food')
+  end
+  
+  def test_carrot_and_dollar
+    assert trex('^foo$','foo')
+  end
+  
+  def test_non_match
+    assert !trex('foo','bar')
+  end
+  
+  def test_non_match_carrot
+    assert !trex('^foo','seafood')
+  end
+  
+  def test_non_match_dollar
+    assert !trex('food$','foodways')
+  end
+  
+  def test_non_match_carrot_dollar
+    assert !trex('^foo$','food')
+  end        
+end    
+
+
