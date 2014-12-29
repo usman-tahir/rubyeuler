36e76f07431e115d383f49825a7a536dbf877f6f
kellyi <kinnes@gmail.com>
2014-12-29 05:39:37 -0500

    find longest hailstone sequence recursively

---
+++ recursive_hailstone.rb
@@ -0,0 +1,20 @@
+# http://rosettacode.org/wiki/Hailstone_sequence
+
+def hailstone(nth,counter=1)
+  if nth == 1
+    counter
+  elsif nth % 2 == 0
+    hailstone(nth/2,counter+1)
+  elsif nth % 2 == 1
+    hailstone(((3*nth)+1),counter+1)
+  end
+end
+
+value = 1
+(1..100_000).each do |n|
+  if hailstone(n) > hailstone(value)
+    value = n
+  end
+end
+
+p value 
