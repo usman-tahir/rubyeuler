691767510aabf0289fcc9770777a3df544e368b1
Kelly Innes <kinnes@gmail.com>
2015-01-07 22:05:30 -0500

    write a function using swift repl

---
+++ interactive_programming.swift
@@ -0,0 +1,7 @@
+Welcome to Swift!  Type :help for assistance.
+1> // http://rosettacode.org/wiki/Interactive_programming
+2> func joinStrings(one:String,two:String) -> String { 
+3.     return "\(one)::\(two)" 
+4. }
+5> joinStrings("Rosetta","Code")
+$R0: String = "Rosetta::Code"
