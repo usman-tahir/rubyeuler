8571578034daf52e5ef85d6c0019afd5d94cd633
Kelly Innes <kinnes@gmail.com>
2015-01-15 18:22:54 -0500

    involved one liner to determine kaprekar numbers

---
+++ kaprekar.rb
@@ -0,0 +1,9 @@
+# http://programmingpraxis.com/2010/09/21/kaprekar-numbers/
+
+def kaprekar(k)
+  (k**2).to_s[-k.to_s.length, k.to_s.length].to_i + (k**2).to_s[0, (k**2).to_s.length-(k**2).to_s[-k.to_s.length, k.to_s.length].length].to_i == k ? true : false
+end
+
+(1..1000).each do |n|
+  p n if kaprekar(n)
+end
