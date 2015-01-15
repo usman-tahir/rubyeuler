f34cd345888ecf34afd50e81748bf27a09f49d30
Kelly Innes <kinnes@gmail.com>
2015-01-15 18:24:07 -0500

    removed unnecessary ternary

--- kaprekar.rb
+++ kaprekar.rb
@@ -1,7 +1,7 @@
 # http://programmingpraxis.com/2010/09/21/kaprekar-numbers/
 
 def kaprekar(k)
-  (k**2).to_s[-k.to_s.length, k.to_s.length].to_i + (k**2).to_s[0, (k**2).to_s.length-(k**2).to_s[-k.to_s.length, k.to_s.length].length].to_i == k ? true : false
+  (k**2).to_s[-k.to_s.length, k.to_s.length].to_i + (k**2).to_s[0, (k**2).to_s.length-(k**2).to_s[-k.to_s.length, k.to_s.length].length].to_i == k
 end
 
 (1..1000).each do |n|
