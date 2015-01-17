d50b53a2ce39492136eafff286566de2e107169f
kellyi <kinnes@gmail.com>
2015-01-16 22:49:37 -0500

    find first 25 vampire numbers plus fangs

---
+++ vampire_numbers.rb
@@ -0,0 +1,47 @@
+# http://rosettacode.org/wiki/Vampire_number
+
+# factorization borrowed from http://rosettacode.org/wiki/Factors_of_an_integer#Ruby
+class Integer
+  def factors
+    1.upto(Math.sqrt(self)).select {|i| (self % i).zero?}.inject([]) do |f, i| 
+      f << self/i unless i == self/i
+      f << i
+    end.sort
+  end
+end
+
+def vampire?(number)
+  digits = number.to_s.split(//).sort
+  return false if digits.count.odd?
+  fangs = []
+  number.factors.select {|fctr| fctr.to_s.length == digits.count/2 }.each do |factor| 
+    f_one, f_two = factor, number/factor
+    next if f_one % 10 == 0 && f_two % 10 == 0
+    f_one_ary, f_two_ary = f_one.to_s.split(//), f_two.to_s.split(//)
+    fangs << [f_one,f_two] if [f_one_ary + f_two_ary].flatten.sort == digits
+  end
+  return false if fangs.empty?
+  fangs.each { |fang| fang.sort! }.uniq
+end
+
+found = 0
+counter = 1000
+until found == 25
+  value = vampire?(counter)
+  if value != false
+    puts "#{counter} : #{value}"
+    found += 1
+  end
+  if counter + 1 == 10_000
+    counter = 100_000
+  elsif counter + 1 == 1_000_000
+    counter = 10_000_00
+  else
+    counter += 1
+  end
+end
+
+[16758243290880, 24959017348650, 14593825548650].each do |n|
+  val = vampire?(n)
+  puts val != false ? "#{n} : #{val}" : "#{val}"
+end
