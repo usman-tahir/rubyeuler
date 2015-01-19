734dde5f4e15c6c0898bb1117cb8283a58f28bbd
Kelly Innes <kinnes@gmail.com>
2015-01-18 19:35:26 -0500

    100 doors with a nested ternary and some blocks

---
+++ new_100doors.rb
@@ -0,0 +1,13 @@
+# http://rosettacode.org/wiki/100_doors
+
+def hundred_doors
+  doors = (1..100).collect { 0 }
+  (1..100).each do |pass|
+    doors.each_index do |index|
+      doors[index] == 0 ? doors[index] = 1 : doors[index] == 1 ? doors[index] = 0 : nil if (index+1) % pass == 0
+    end
+  end
+  doors
+end
+
+hundred_doors.each_index { |index| p "#{index+1}" if hundred_doors[index] == 1 }
