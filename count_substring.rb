28cb7fbe0654b30dac7117e00557371e05eb8696
Kelly Innes <kinnes@gmail.com>
2015-01-21 20:12:36 -0500

    count occurences of a substring in a string

---
+++ count_substring.rb
@@ -0,0 +1,19 @@
+# http://rosettacode.org/wiki/Count_occurrences_of_a_substring
+
+def count_substring(string,substring)
+  index = 0
+  counter = 0
+  until index >= string.length
+    if string.slice(index,substring.length) == substring
+      counter += 1
+      index += substring.length
+    else
+      index += 1
+    end
+  end
+  counter
+end
+
+p count_substring("the three truths","th")
+
+p count_substring("ababababab","abab")
