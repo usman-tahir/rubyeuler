0ba33fcc9e1b79ee886d48cd5059c1efcd4897bc
kellyi <kinnes@gmail.com>
2015-01-23 17:50:48 -0500

    primality by trial division in swift

---
+++ trial_division.swift
@@ -0,0 +1,15 @@
+// http://rosettacode.org/wiki/Primality_by_trial_division
+import Darwin
+
+func isPrime(n:Int) -> Bool {
+  for var i = 3; i < n; i = i + 2 {
+    if n % i == 0 {
+      return false
+    }
+  }
+  return true
+}
+
+println(isPrime(37))
+println(isPrime(38))
+println(isPrime(39))
