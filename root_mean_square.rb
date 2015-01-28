cfc1515ff35b4dff8c07579486b46c55581da4bb
Kelly Innes <kinnes@gmail.com>
2015-01-28 17:34:19 -0500

    refactored into one liner

--- root_mean_square.rb
+++ root_mean_square.rb
@@ -3,13 +3,7 @@
 # Compute the Root mean square of the numbers 1..10. 
 
 def root_mean_square(low,high)
-  numerator_array = (low..high).to_a
-  numerator_array.each_index do |index|
-    numerator_array[index] = numerator_array[index]**2
-  end
-  numerator = numerator_array.inject(:+)
-  denominator = numerator_array.count
-  Math.sqrt((numerator.to_r/denominator.to_r))
+  Math.sqrt((low..high).to_a.map { |element| element ** 2 }.inject(:+).to_r / (low..high).to_a.count.to_r)
 end
 
-puts root_mean_square(1,10).inspect
+p root_mean_square(1,10)
