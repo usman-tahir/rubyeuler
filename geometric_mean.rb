82bdbef10d97ae302febfc548b633ac1848398db
Kelly Innes <kinnes@gmail.com>
2015-01-28 17:41:07 -0500

    refactored into one liner

--- geometric_mean.rb
+++ geometric_mean.rb
@@ -1,11 +1,7 @@
 # http://rosettacode.org/wiki/Averages/Pythagorean_means
 
 def geometric_mean(low,high)
-  find_nth_root((low..high).to_a.inject(:*),(low..high).count)
-end
-
-def find_nth_root(val,nth)
-  val ** (1.0 / nth)
+  (low..high).to_a.inject(:*) ** (1.0 / (low..high).count)
 end
 
 p geometric_mean(1,10)
