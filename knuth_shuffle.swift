c7fa36f52292240897e3a82e6e9c3baa574089af
Kelly Innes <kinnes@gmail.com>
2015-01-27 20:35:33 -0500

    knuth shuffle in swift

---
+++ knuth_shuffle.swift
@@ -0,0 +1,16 @@
+// http://rosettacode.org/wiki/Knuth_shuffle
+import Darwin
+
+func knuthShuffle(array:[Int]) -> [Int] {
+  var mutableArray:[Int] = array
+  for var i = 1; i < array.count; i++ {
+    let j = Int(arc4random_uniform(UInt32(array.count)))
+    let k = mutableArray[i]
+    mutableArray[i] = mutableArray[j]
+    mutableArray[j] = k
+  }
+  return mutableArray
+}
+
+let testArray = [0,1,2,3,4,5,6,7,8,9]
+println(knuthShuffle(testArray))
