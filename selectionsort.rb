1920ed3c922987e5a15bc19ab5cb2457d03df23e
Kelly Innes <kinnes@gmail.com>
2015-01-29 21:03:46 -0500

    selection sort refactored into one-liner

--- selectionsort.rb
+++ selectionsort.rb
@@ -1,19 +1,11 @@
 # http://rosettacode.org/wiki/Sorting_algorithms/Selection_sort
-start_time = Time.now
-array_to_sort1 = [26, 45, 4, 98, 769, 234, 24324, 78, 865, 0, 6, 123, 7, 14345, 5, 12, 211, 1, 3215, 67, 8, 90, 9, 2345, 456]
-array_to_sort2 = [7,3,1,5,10,0,4]
-test_array = array_to_sort1
 
-def selection_sort(array_to_sort)
-  array_to_sort.each_index do |index|
-    min = index
-    while min < array_to_sort.count
-      array_to_sort[index], array_to_sort[min] = array_to_sort[min], array_to_sort[index] if array_to_sort[min] < array_to_sort[index]
-      min += 1
-    end
-  end
-  array_to_sort		
+array1 = [26, 45, 4, 98, 769, 234, 24324, 78, 865, 0, 6, 123, 7, 14345, 5, 12, 211, 1, 3215, 67, 8, 90, 9, 2345, 456]
+array2 = [7,3,1,5,10,0,4]
+
+def selection_sort(ary)
+  ary.each_index { |i| (i...ary.count).each { |m| ary[i], ary[m] = ary[m], ary[i] if ary[m] < ary[i] } }
 end
 
-puts selection_sort(test_array).inspect
-puts ((Time.now - start_time).to_f).to_s + "s"
+p selection_sort(array1)
+p selection_sort(array2)
