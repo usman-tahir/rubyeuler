8fd57975992de49a5673b294711ccdd909c14856
Kelly Innes <kinnes@gmail.com>
2015-02-08 15:18:20 -0500

    find a number's digital root in c

---
+++ digital_root.c
@@ -0,0 +1,30 @@
+// http://rosettacode.org/wiki/Digital_root
+
+#include <stdio.h>
+
+int sumDigits(int n, int counter)
+{
+    if (n < 10) {
+        return n + counter;
+    } else {
+        counter = counter + (n % 10);
+        n /= 10;
+        return sumDigits(n, counter);
+    }
+}
+
+int findRoot(int number)
+{
+    if (number < 10) {
+        return number;
+    } else {
+        number = sumDigits(number, 0);
+        return findRoot(number);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int value = findRoot(961038);
+    printf("%d\n", value);
+    return 0;
+}
