6abade9278f5b5fb14b70ae257cb71fabfd9d950
Kelly Innes <kinnes@gmail.com>
2015-02-27 17:13:51 -0500

    cut some lines

--- currency_exchange.rb
+++ currency_exchange.rb
@@ -3,11 +3,8 @@ require 'net/http'
 require 'json'
 
 def convert_btc_usd(number,original)
-  source = "https://api.bitcoinaverage.com/ticker/global/USD/"
-  response = Net::HTTP.get_response(URI.parse(source))
-  data = response.body
-  current_data = JSON.parse(data)
-  btc_price_in_usd = current_data["24h_avg"] 
+  response = Net::HTTP.get_response(URI.parse("https://api.bitcoinaverage.com/ticker/global/USD/"))
+  btc_price_in_usd = JSON.parse(response.body)["24h_avg"]
   original == "usd" ? number * btc_price_in_usd : number / btc_price_in_usd
 end
 
