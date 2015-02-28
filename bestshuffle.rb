d36615fcc88335eb2ceba1a8e100689d1954e2f3
Kelly Innes <kinnes@gmail.com>
2015-02-28 11:23:05 -0500

    cleaning up

--- bestshuffle.rb
+++ bestshuffle.rb
@@ -1,14 +1,14 @@
 # http://rosettacode.org/wiki/Best_shuffle
 
-def best_shuffle(word,shuffled_word=[])
-  chars = word.split(//)
-  counter = 0; compare = false
-  until compare == true
-    counter = 0; shuffled_word = word.split(//).shuffle
-    chars.each_index {|i| counter += 1 if shuffled_word[i] == chars[i]}
-    compare = true if counter == 0 || chars.count < 2 || (chars.count > chars.uniq.count * 3 && counter < chars.count)
+def best_shuffle(word)
+  chars = word.split(//); shuf_chars=[]
+  c = 0; comp = false
+  until comp == true
+    c = 0; shuf_chars = word.split(//).shuffle
+    chars.each_index {|i| c += 1 if shuf_chars[i] == chars[i]}
+    comp = true if c == 0 || chars.count < 2 || (chars.count > chars.uniq.count * 3 && c < chars.count)
   end
-  puts "#{word}, #{shuffled_word.join}, #{counter}"
+  puts "#{word}, #{shuf_chars.join}, #{c}"
 end
 
 ["abracadabra", "seesaw", "elk", "grrrrrr", "up", "a"].each {|word| best_shuffle(word)}
