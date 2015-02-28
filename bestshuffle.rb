c9cf7420c3ba30cb2c8234cfdc8a8f7f15ca3599
Kelly Innes <kinnes@gmail.com>
2015-02-28 11:23:35 -0500

    cleaning up

--- bestshuffle.rb
+++ bestshuffle.rb
@@ -4,7 +4,7 @@ def best_shuffle(word)
   chars = word.split(//); shuf_chars=[]
   c = 0; comp = false
   until comp == true
-    c = 0; shuf_chars = word.split(//).shuffle
+    c = 0; shuf_chars = chars.shuffle
     chars.each_index {|i| c += 1 if shuf_chars[i] == chars[i]}
     comp = true if c == 0 || chars.count < 2 || (chars.count > chars.uniq.count * 3 && c < chars.count)
   end
