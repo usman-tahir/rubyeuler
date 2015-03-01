3740a278c1d16c0e6a5dfa568f4584e6b8e6f676
Kelly Innes <kinnes@gmail.com>
2015-03-01 17:27:59 -0500

    some cuts

--- luhn.pl
+++ luhn.pl
@@ -3,32 +3,22 @@ use strict;
 use warnings;
 
 sub checksum {
-  my $n = shift;
-  my @list = split(//,reverse $n);
-  my $s_one = 0;
+  my @list = split(//,reverse shift);
+  my $acc = 0;
   for (my $i = 0; $i < scalar @list; $i += 2) {
-    $s_one = $s_one + $list[$i];
+    $acc += $list[$i];
   }
-  my $s_two = 0;
   for (my $j = 1; $j < scalar @list; $j += 2) {
-    my $value = $list[$j] * 2;
-    if ($value >= 10) {
-      $s_two += two_digit($value);
-    } else {
-      $s_two += $value;
-    }
+    $list[$j] *= 2;
+    $acc += two_digit($list[$j]) if $list[$j] >= 10;
+    $acc += $list[$j] if $list[$j] < 10;
   }
-  return ($s_one + $s_two) % 10 == 0;
+  return $acc % 10 == 0;
 }
 
 sub two_digit {
-  my $n = shift;
-  my @list = split(//,$n);
-  my $val = 0;
-  for (my $i = 0; $i < scalar @list; $i++) {
-    $val += $list[$i]
-  }
-  return $val;
+  my @list = split(//,shift);
+  return $list[0] + $list[-1];
 }
 
 foreach (49927398716, 49927398717, 1234567812345678, 1234567812345670) {
