d09cadbdbf00e5857beb701f7327c12120d516f6
Kelly Innes <kinnes@gmail.com>
2015-03-04 18:06:02 -0500

    refactoring

--- nth.rb
+++ nth.rb
@@ -1,30 +1,14 @@
 # http://rosettacode.org/wiki/N%27th
-start_time = Time.now
 
-def nth(number)
-  postfix = "th"
-  number = number.to_s
-  number.length
-  if number[-2] != "1"
-    case
-    when number[-1] == "1" then postfix = "st"
-    when number[-1] == "2" then postfix = "nd"
-    when number[-1] == "3" then postfix = "rd"
-    end
+def nth(n)
+  unless n.to_s[-2] == "1"
+    return n.to_s << "st" if n.to_s[-1] == "1"
+    return n.to_s << "nd" if n.to_s[-1] == "2"
+    return n.to_s << "rd" if n.to_s[-1] == "3"
   end
-  number << postfix	
+  return n.to_s << "th"
 end
 
-def find_nth(start_of_range, end_of_range)
-  nths =[]
-  (start_of_range..end_of_range).each do |number|
-    nths << nth(number)
-  end
-  nths
-end		
-
-puts find_nth(0,25).inspect
-puts find_nth(250,265).inspect
-puts find_nth(1000,1025).inspect
-
-puts ((Time.now-start_time).to_f).to_s + "s"
+(0..25).each {|n| puts nth(n)}
+(250..265).each {|n| puts nth(n)}
+(1000..1025).each {|n| puts nth(n)}
