b38e88b5d87bfa01a4ae7206ef791b203c95b713
Kelly Innes <kinnes@gmail.com>
2015-03-05 08:35:43 -0500

    refactoring

--- selfdescribingnumber.rb
+++ selfdescribingnumber.rb
@@ -10,33 +10,14 @@
 # value 0 because there are not 1's in the number. Position "2" has value 2 
 # and there is two 2. And the position "3" has value 0 and there are zero 3's. 
 
-start_time = Time.now
 t_test_array = [1210,2020,21200,3211000,42101000]
 f_test_array = [3333,3,2021,11200,45,98]
 
-def self_describing?(number)
-  number_string = number.to_s
-  num_s_length = number_string.length
-  number_array = number_string.split(//).map {|digit| digit.to_i}
-  number_index_counter = 0
-  number_test =[]
-  until number_index_counter == num_s_length
-    number_test << true if number_array.count(number_index_counter) == number_string[number_index_counter].to_i 
-    number_test << false if number_array.count(number_index_counter) != number_string[number_index_counter].to_i 
-    number_index_counter += 1
-  end
-  return false if number_test.include?(false)
-  return true if !number_test.include?(false)
+def self_describing(n)
+  digits = n.to_s.split(//).map {|d| d.to_i}
+  digits.each_index {|i| return false unless digits.count(i) == digits[i]}
+  return true
 end
 
-def self_desc_test(test_array)
-  test_array.each do |number|
-    puts number.to_s + " is self-describing." if self_describing?(number) == true
-    puts number.to_s + " is NOT self-describing." if self_describing?(number) == false
-  end
-end
-
-self_desc_test(t_test_array)
-self_desc_test(f_test_array)  	
- 
-puts ((Time.now-start_time).to_f).to_s + "s"
+t_test_array.each {|n| puts "#{n}: #{self_describing(n)}"}
+f_test_array.each {|n| puts "#{n}: #{self_describing(n)}"}
