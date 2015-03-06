37802164b6858d0f557597049a5156a6565672cb
Kelly Innes <kinnes@gmail.com>
2015-03-06 08:51:38 -0500

    find numbers divisible by only 3,5,7 in perl

---
+++ 357_numbers.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub t_f_s {
+  my $n = shift;
+  $n /= 3 while $n % 3 == 0;
+  $n /= 5 while $n % 5 == 0;
+  $n /= 7 while $n % 7 == 0;
+  return $n == 1;
+}
+
+my $c = 0;
+for (my $i = 1; $i < 1_000_000; $i += 2) {
+  $c++ if t_f_s($i);
+}
+print "$c\n";
