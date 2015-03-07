17515f42e3b52b0e952043c9e0e44f0847562a07
Kelly Innes <kinnes@gmail.com>
2015-03-07 10:22:42 -0500

    third binary search implementation as part of a code kata

--- karate_chop.rb
+++ karate_chop.rb
@@ -3,13 +3,9 @@
 def b_one(array,element,low,high)
   return nil if high < low
   m = (low + high) / 2
-  if array[m] == element
-    m
-  elsif array[m] > element
-    b_one(array,element,low,m-1)
-  else array[m] < element
-    b_one(array,element,m+1,high)
-  end
+  return m if array[m] == element
+  return b_one(array,element,low,m-1) if array[m] > element
+  return b_one(array,element,m+1,high) if array[m] < element
 end
 # errors encountered with first: stack level too deep without using low/high arguments
 
@@ -18,20 +14,29 @@ def b_two(array,element)
   while array[mid] != element
     mid -= 1 if array[mid] > element
     mid += 1 if array[mid] < element
-    if array[mid+1] > element && array[mid] < element
-      mid = nil
-      break
-    end
-    if array[mid] > element && array[mid-1] < element
-      mid = nil
-      break
-    end
+    return nil if array[mid+1] > element && array[mid] < element
+    return nil if array[mid] > element && array[mid-1] < element
   end
   mid
 end
 #slower, duplicates code in the checks for mid > element &&c
+#also, doesn't divide and conquer after the first step
+
+def b_three(array,element)
+  low = 0; high = array.count; mid = high/2
+  until array[mid] == element
+    if element < array[mid] then high = mid; mid = (low + mid)/2 end
+    if element > array[mid] then low = mid; mid = (mid + high)/2 end
+    return nil if array[mid+1] > element && array[mid] < element
+    return nil if array[mid] > element && array[mid-1] < element
+  end
+  mid
+end
+#still iterative; does repeatedly split list in half
+#slightly more verbose, but probably clearer to read
 
 test = [0,1,4,5,6,7,8,9,12,26,45,67,78,90,98,123,211,234,456,769,865,2345,3215,14345,24324]
 
 p b_one(test,98, 0, test.count)
 p b_two(test,98)
+p b_three(test,98)
