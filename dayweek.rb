da27308b8f199143d3b8f699f94f3080ff4bb4f1
kellyi <kinnes@gmail.com>
2015-03-07 15:35:33 -0500

    cleaned up with a proc

--- dayweek.rb
+++ dayweek.rb
@@ -1,4 +1,5 @@
 # http://rosettacode.org/wiki/Day_of_the_week
 # In what years between 2008 and 2121 will the 25th of December be a Sunday?
 
-(2008..2121).each {|y| xmas = Time.new(y, 12, 25); puts xmas.strftime("%m/%d/%Y") if xmas.sunday? }
+day = -> n { Time.new(n,12,25) }
+(2008..2121).each {|y| puts day[y].strftime("%m/%d/%Y") if day[y].sunday? }
