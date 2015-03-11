39d9a10c83ba7509c2e7e45c65147d590ab860f0
Kelly Innes <kinnes@gmail.com>
2015-03-11 02:08:27 -0400

    refactored into one-liner

--- recursum.rb
+++ recursum.rb
@@ -1,15 +1,8 @@
 # recursively sums an array
-# from _Think Like A Programmer_ chapter on recursion
-start_time = Time.now
-test_array = [1,2,3,4,5]
 
-def recursum(array, array_size)
-  return 0 if array_size == 0
-  last_number = array[array_size-1]
-  all_but_last_sum = recursum(array, array_size-1)
-  return last_number + all_but_last_sum
+def recursum(array,index=0)
+   index == array.count ? 0 : array[index] + recursum(array,index+1)
 end
 
-puts recursum(test_array, test_array.count)	
-
-puts ((Time.now - start_time).to_f).to_s + "s"
+test_array = [1,2,3,4,5]
+p recursum(test_array)	
