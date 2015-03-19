4f1c5953340bea75ab89cae7eb3ef73caf4c89b6
Kelly Innes <kinnes@gmail.com>
2015-03-19 17:24:39 -0400

    find upside up numbers below 10_000

---
+++ upside_up_numbers.rb
@@ -0,0 +1,20 @@
+# http://programmingpraxis.com/2011/05/27/upside-up/
+
+DIGITS = { 0 => 0, 1 => 1, 6 => 9, 8 => 8, 9 => 6 }
+
+def upside_up(n)
+  (0...n.to_s.length).each { |i| return false unless DIGITS[n.to_s[i].to_i] == n.to_s[-(i+1)].to_i }
+  true
+end
+
+def find_next(counter = 1962)
+  counter += 1 until upside_up(counter)
+  return counter if upside_up(counter)
+end
+
+def count_below_ten_thousand
+  (0...10_000).to_a.map { |n| upside_up(n) ? n : nil }.compact.count
+end
+
+p find_next
+p count_below_ten_thousand
