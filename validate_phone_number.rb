93e84b0f55d3e95e9248d3f03c57a8af6644800b
Kelly Innes <kinnes@gmail.com>
2015-03-20 18:03:53 -0400

    validate a phone number in ruby with tests

---
+++ validate_phone_number.rb
@@ -0,0 +1,25 @@
+# http://programmingpraxis.com/2011/12/13/validating-telephone-numbers/
+require 'minitest/autorun'
+
+def validate_phone_number(n)
+  n.gsub!(/[-.() ]/, "")
+  if n == nil || n.length != 7 && n.length != 10
+    return "#{n} is not valid"
+  end
+  return "#{n} is valid"
+end
+
+class PhoneNumberValidatorTests < MiniTest::Test
+
+  def test_valid
+    val = ["1234567890","123-456-7890","123.456.7890","(123)456-7890",
+          "(123) 456-7890","456-7890"]
+    val.each { |n| assert_equal(validate_phone_number(n),"#{n} is valid") }
+  end
+  
+  def test_not_valid
+    inval = ["123-45-6789","123:4567890","123/456-7890"]
+    inval.each { |n| assert_equal(validate_phone_number(n),"#{n} is not valid") }
+  end
+
+end
