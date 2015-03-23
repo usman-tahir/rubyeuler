5443717895257e89712601e2cb2f5fbb097e000c
Kelly Innes <kinnes@gmail.com>
2015-03-22 20:49:51 -0400

    refactored into proc

--- dotproduct.rb
+++ dotproduct.rb
@@ -1,11 +1,9 @@
 # http://rosettacode.org/wiki/Dot_product
 # http://en.wikipedia.org/wiki/Dot_product
 
-def dot_product(one,two)
-  Array.new(one.count,nil).each_with_index.map {|x,i| x = one[i] * two[i]}.inject(:+)
-end
+dot_product = -> one,two { Array.new(one.count,nil).each_with_index.map {|x,i| x = one[i] * two[i]}.inject(:+) }
 
 test_array1 = [1,3,-5]
 test_array2 = [4,-2,-1]
 
-puts dot_product(test_array1,test_array2)
+puts dot_product[test_array1,test_array2]
