320b508bf5577c5050ec071c64d41d5a792d0d91
kellyi <kinnes@gmail.com>
2015-03-23 19:51:34 -0400

    refactored find first 15 catalan numbers in ruby

--- catalannumbers.rb
+++ catalannumbers.rb
@@ -1,35 +1,12 @@
-# http://rosettacode.org/wiki/Catalan_numbers/Pascal%27s_triangle
-# The task is to print out the first 15 Catalan numbers by extracting them from Pascal's triangle
-start_time = Time.now
+# http://rosettacode.org/wiki/Catalan_numbers/Pascal's_triangle
 
-def pascals_triangle(row_to_find)
-  triangle_for_row = [1]
-  fractions = []
-  number_of_rows_counter = row_to_find
-  denominator_counter = 1
-  until number_of_rows_counter == 0
-    fractions << (number_of_rows_counter.to_r/denominator_counter.to_r)
-    number_of_rows_counter -= 1
-    denominator_counter += 1
-  end	
-  fractions.each do |fraction|
-    triangle_for_row << (fraction * triangle_for_row[-1]).to_i
+def catalan_numbers(n)
+  # translation of http://rosettacode.org/wiki/Catalan_numbers/Pascal%27s_triangle#Perl
+  t = [0,1]
+  (1..n).each do |i|
+    i.downto(1).each { |j| t[j] += t[j-1] }; t[i+1] = t[i]
+    (i+1).downto(1).each { |j| t[j] += t[j-1] }; puts "#{t[i+1] - t[i]}"
   end
-  triangle_for_row	
 end
 
-def catalan_numbers(number_to_find)
-  catalan_numbers_array = []
-  counter = 2
-  until catalan_numbers_array.count == number_to_find
-    pascal_row = pascals_triangle(counter)
-    mid_index = pascal_row.find_index(pascal_row.max)
-    catalan_numbers_array << (pascal_row[mid_index] - pascal_row[mid_index+1])
-    counter += 2
-  end
-  catalan_numbers_array
-end
-
-puts catalan_numbers(15).inspect
-
-puts ((Time.now-start_time).to_f).to_s + "s"
+catalan_numbers(15)
