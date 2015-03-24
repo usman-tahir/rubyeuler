c28e1b1234b2f6d3cb4db9fa430a5fe06c415f84
kellyi <kinnes@gmail.com>
2015-03-23 20:04:28 -0400

    refactored into three one-liners

--- digitalroot.rb
+++ digitalroot.rb
@@ -1,26 +1,11 @@
 # http://rosettacode.org/wiki/Digital_root
-start_time = Time.now
-test_numbers_array = [627615, 39390, 588225, 393900588225]
 
-def find_digital_root(number)
-  number_length = number.to_s.length
-  additive_persistence_counter = 0
-  until number_length == 1
-    number = number_split(number)
-    additive_persistence_counter += 1
-    number_length = number.to_s.length
-  end
-  digital_root = [number, additive_persistence_counter]
-end	
-
-def number_split(number)
-  digit_array = number.to_s.split(//).map {|digit| digit.to_i}
-  digit_array.inject(:+)
+def sum_digits(n,counter=0)
+  n < 10 ? n + counter : sum_digits(n/10,counter+(n%10))
 end
 
-test_numbers_array.each do |test_number|
-  puts test_number.to_s + " has a digital root of " + find_digital_root(test_number)[0].to_s
-  puts test_number.to_s + " has additive persistence of " + find_digital_root(test_number)[1].to_s
+def find_root(n,persistence=1)
+  n < 10 ? [n,persistence] : find_root(sum_digits(n),persistence+1)
 end
 
-puts ((Time.now-start_time).to_f).to_s + "s"
+[627615, 39390, 588225, 393900588225].each {|t| puts "#{t}: root = #{find_root(t)[0]} // persistence = #{find_root(t)[1]}" }
