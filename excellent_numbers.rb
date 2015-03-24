1831c17aaaa4d4ad73d5191142a37e97337eac85
kellyi <kinnes@gmail.com>
2015-03-24 18:27:09 -0400

    translate python version into ruby

--- excellent_numbers.rb
+++ excellent_numbers.rb
@@ -5,4 +5,31 @@ def excellent?(n)
   partitions[1] - partitions[0] == n
 end
 
-[140400, 190476, 216513, 300625, 334668, 416768, 484848, 530901].each { |n| puts "#{n}: #{excellent?(n)}" }
+def find_excellent
+  n = 100_000
+  
+  def f(a)
+    a * (a + 100_000)
+  end
+
+  def g(b)
+    b * (b - 1)
+  end
+
+  a = n/10
+  b = 1
+  accumulator = 0
+  while (a < n) && (b < n)
+    if f(a) < g(b)
+      a += 1
+    elsif f(a) > g(b)
+      b += 1
+    else
+      accumulator += a * n + b
+      a += 1; b += 1
+    end
+  end
+  accumulator
+end
+
+p find_excellent
