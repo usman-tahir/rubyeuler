f34c759896b0412354c761c894ed7bb445c6917d
kellyi <kinnes@gmail.com>
2015-03-24 18:34:01 -0400

    refactoring into one liners

--- excellent_numbers.rb
+++ excellent_numbers.rb
@@ -1,34 +1,11 @@
 # http://programmingpraxis.com/2015/03/24/excellent-numbers/
 
-def excellent?(n)
-  partitions = n.to_s.scan(/.{#{n.to_s.length/2}}/).map { |e| e.to_i ** 2 }
-  partitions[1] - partitions[0] == n
-end
-
 def find_excellent
   n = 100_000
-  
-  def f(a)
-    a * (a + 100_000)
-  end
-
-  def g(b)
-    b * (b - 1)
-  end
-
-  a = n/10
-  b = 1
-  accumulator = 0
-  while (a < n) && (b < n)
-    if f(a) < g(b)
-      a += 1
-    elsif f(a) > g(b)
-      b += 1
-    else
-      accumulator += a * n + b
-      a += 1; b += 1
-    end
-  end
+  f = -> a { a * (a + n) }
+  g = -> b { b * (b - 1) }
+  a = n/10; b = 1; accumulator = 0
+  ( f[a] < g[b] ? a += 1 : f[a] > g[b] ? b += 1 : (accumulator += a * n + b; a += 1; b += 1) ) while (a < n) && (b < n)
   accumulator
 end
 
