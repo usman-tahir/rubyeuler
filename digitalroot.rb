5e6a3094c6d4e66982cb11b2265126f7dbc352a9
kellyi <kinnes@gmail.com>
2015-03-23 20:05:12 -0400

    extra space

--- digitalroot.rb
+++ digitalroot.rb
@@ -8,4 +8,4 @@ def find_root(n,persistence=1)
   n < 10 ? [n,persistence] : find_root(sum_digits(n),persistence+1)
 end
 
-[627615, 39390, 588225, 393900588225].each {|t| puts "#{t}: root = #{find_root(t)[0]} // persistence = #{find_root(t)[1]}" }
+[627615, 39390, 588225, 393900588225].each { |t| puts "#{t}: root = #{find_root(t)[0]} // persistence = #{find_root(t)[1]}" }
