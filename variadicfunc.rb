122e8067cbfd3d62270a3fff37bdbf2e322bf5bf
kellyi <kinnes@gmail.com>
2015-03-24 20:19:28 -0400

    refactored into two lines

--- variadicfunc.rb
+++ variadicfunc.rb
@@ -1,25 +1,8 @@
 # http://rosettacode.org/wiki/Variadic_function
-start_time = Time.now
 
 def cardinal_to_ordinal(*numbers)
-  ordinals = []
-  numbers.each do |number|
-    postfix = "th"
-    number = number.to_s
-    if number[-2] != "1"
-      case
-      when number[-1] == "1" then postfix = "st"
-      when number[-1] == "2" then postfix = "nd"
-      when number[-1] == "3" then postfix = "rd"
-      end
-    end
-    ordinals << (number << postfix)
-  end
-  ordinals
+  suff = { "1" => "st", "2" => "nd", "3" => "rd" }
+  numbers.map { |e| e.to_s }.each { |n| n[-2] == "1" ? n << "th" : suff.include?(n[-1]) ? n << suff[n[-1]] : n << "th" }
 end
 
 puts cardinal_to_ordinal(1,11,21,34,114,200_112,1_000_023)
-
-puts ((Time.now - start_time).to_f).to_s + "s"    	
-
-
