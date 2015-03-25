76d74521156552ebd5fd327f752bcd3db3837b3f
kellyi <kinnes@gmail.com>
2015-03-24 20:23:03 -0400

    cleaning up ternary

--- variadicfunc.rb
+++ variadicfunc.rb
@@ -2,7 +2,7 @@
 
 def cardinal_to_ordinal(*numbers)
   suff = { "1" => "st", "2" => "nd", "3" => "rd" }
-  numbers.map { |e| e.to_s }.each { |n| n[-2] == "1" ? n << "th" : suff.include?(n[-1]) ? n << suff[n[-1]] : n << "th" }
+  numbers.map { |e| e.to_s }.each { |n| n[-2] == "1" || !suff.include?(n[-1]) ? n << "th" : n << suff[n[-1]] }
 end
 
 puts cardinal_to_ordinal(1,11,21,34,114,200_112,1_000_023)
