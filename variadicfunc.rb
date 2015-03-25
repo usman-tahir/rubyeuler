01e0474daf666c96f3a0db044ee6c97238816c09
Kelly Innes <kinnes@gmail.com>
2015-03-24 20:58:41 -0400

    redid with a proc

--- variadicfunc.rb
+++ variadicfunc.rb
@@ -1,8 +1,5 @@
 # http://rosettacode.org/wiki/Variadic_function
 
-def cardinal_to_ordinal(*numbers)
-  suff = { "1" => "st", "2" => "nd", "3" => "rd" }
-  numbers.map { |e| e.to_s }.each { |n| n[-2] == "1" || !suff.include?(n[-1]) ? n << "th" : n << suff[n[-1]] }
-end
-
-puts cardinal_to_ordinal(1,11,21,34,114,200_112,1_000_023)
+suff = { "1" => "st", "2" => "nd", "3" => "rd" }
+func = -> *n { n.map { |e| e.to_s }.each { |n| n[-2] == "1" || !suff.include?(n[-1]) ? n << "th" : n << suff[n[-1]] } }
+puts func[1,11,21,34,114,200_112,1_000_023]
