6a78d47ab20a10059f7cd1451bab5f141fc8e1a5
kellyi <kinnes@gmail.com>
2015-03-28 01:05:43 -0400

    refactor into one-liner

--- neweuler1.rb
+++ neweuler1.rb
@@ -1,11 +1,3 @@
 # http://projecteuler.net/problem=1
 
-def find_below(number)
-  result = 0
-  (1...number).each do |int|
-    result += int if int % 3 == 0 || int % 5 == 0
-  end
-  result
-end
-
-puts find_below(1000)     
+puts (1...1000).map { |n| n % 5 == 0 ? n : n % 3 == 0 ? n : nil }.compact.inject(:+) 
