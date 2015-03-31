2d5ce2c5c16713e2872978157a14b12796d9d44f
Kelly Innes <kinnes@gmail.com>
2015-03-31 17:48:26 -0400

    find derangements of a list and subfactorial of an int

--- derangement.rb
+++ derangement.rb
@@ -1,26 +1,13 @@
 # http://rosettacode.org/wiki/Permutations/Derangements
-start_time = Time.now
 
-def derangement(start_number,end_number)
-  derangement_base_array = (start_number..end_number).to_a
-  permutations = derangement_base_array.permutation.to_a
-  3.times do 
-    permutations.each do |permutation|
-      permutation.each_index do |permutation_index|
-        permutations.delete(permutation) if permutation[permutation_index] == derangement_base_array[permutation_index]
-      end
-    end
-  end
-  permutations
+def derangement(list)
+  list.permutation.to_a.each {|p| list.each_index {|i| p[i] = nil if list[i] == p[i]}}.reject {|p| p.include?(nil)}
 end
 
-def deranged_number_count(start_number,end_number)
-  derangement(start_number,end_number).count
+def subfactorial(n)
+  n = derangement((1..n).to_a).count
 end
 
-puts derangement(0,2).inspect
-puts derangement(1,4).inspect
-puts deranged_number_count(1,4)
-
-puts ((Time.now - start_time).to_f).to_s + "s"
-
+p derangement([0,1,2,3])
+p derangement((0..9).to_a).count
+p subfactorial(10)
