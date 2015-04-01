417cc4ccdf14b177bdf63b49f4bb42fdbc26c86f
Kelly Innes <kinnes@gmail.com>
2015-04-01 19:22:15 -0400

    refactored

--- needlehaystack.rb
+++ needlehaystack.rb
@@ -1,19 +1,10 @@
 # http://rosettacode.org/wiki/Search_a_list
-start_time = Time.now
+
+def find_needle(array,item,i=0)
+  i == array.count ? "nil" : array[i] == item ? i : find_needle(array,item,i+1)
+end
+
 haystack = ["Zig", "Zag", "Wally", "Ronald", "Bush", "Krusty", "Charlie", "Bush", "Boz", "Zag"]
-# copied from ActionScript example http://rosettacode.org/wiki/Search_a_list#ActionScript
-needle_to_seek = "Zag"
+needle = "Zog"
 
-def find_needle(array, string)
-  if array.count(string) > 1 
-    array.rindex(string) # return highest index
-    # array.index(string) # return lowest index
-  elsif array.include?(string)
-    array.index(string)
-  else
-    return "NOT FOUND"
-  end	
-end	
-
-puts "#{needle_to_seek} is at index " + find_needle(haystack, needle_to_seek).to_s
-puts ((Time.now-start_time).to_f).to_s + "s"
+puts "#{needle} at index: #{find_needle(haystack, needle)}"
