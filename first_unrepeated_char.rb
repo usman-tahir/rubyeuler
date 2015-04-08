51254eff5a9b366a9e0ed676536174bf07a6af3f
kellyi <kinnes@gmail.com>
2015-04-08 19:08:37 -0400

    turned into proc

--- first_unrepeated_char.rb
+++ first_unrepeated_char.rb
@@ -1,7 +1,5 @@
 # http://programmingpraxis.com/2013/04/30/first-unrepeated-character-in-a-string/
 
-def find_first_unrepeated_char(string)
-  (0...string.length).each {|i| return string[i] if string.count(string[i]) == 1}
-end
+unrepeated = -> string { (0...string.length).each { |i| return string[i] if string.count(string[i]) == 1 } }
 
-p find_first_unrepeated_char("aabbcddd")
+p unrepeated["aabbcddd"]
