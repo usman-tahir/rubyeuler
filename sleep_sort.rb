8c78f9dbc7e7bd4baf3218816448994471c4f2f2
Kelly Innes <kinnes@gmail.com>
2015-05-07 17:52:54 -0400

    done as lambda expression

--- sleep_sort.rb
+++ sleep_sort.rb
@@ -1,9 +1,9 @@
 # http://rosettacode.org/wiki/Sorting_algorithms/Sleep_sort
 require 'thread'
 
-def sleep_sort(array,l=[])
-  array.map {|n| Thread.new { sleep n/100.to_f; Mutex.new.synchronize { l << n } } }.each(&:join)
-  return l
-end  
+sleep_sort = -> a, l=[] {
+  a.map {|n| Thread.new { sleep n/100.to_f; 
+  Mutex.new.synchronize { l << n } } }.each(&:join)
+  return l }
 
-p sleep_sort(ARGV.map {|arg| arg.to_i})
+p sleep_sort[ARGV.map {|arg| arg.to_i}]
