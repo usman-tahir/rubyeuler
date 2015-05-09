0b68d04753df08f418e6d7f9a1c97bd37f5bc475
kellyi <kinnes@gmail.com>
2015-05-08 21:09:55 -0400

    monkeys and coconuts problem solved with help from the integer sequence encyclopedia

---
+++ monkeys_and_coconuts.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# http://programmingpraxis.com/2015/05/08/monkeys-and-coconuts/
+# http://oeis.org/A002021
+
+nuts = -> n { n % 2 == 0 ? (n - 1) * (n ** n - 1) : n ** n - n + 1 }
+(1..30).each { |s| puts "#{s}: #{nuts[s]}" }
