9ee1f11275a35c60fafcc941da330a121f314c69
kellyi <kinnes@gmail.com>
2015-05-08 21:10:54 -0400

    used native even? method

--- monkeys_and_coconuts.rb
+++ monkeys_and_coconuts.rb
@@ -2,5 +2,5 @@
 # http://programmingpraxis.com/2015/05/08/monkeys-and-coconuts/
 # http://oeis.org/A002021
 
-nuts = -> n { n % 2 == 0 ? (n - 1) * (n ** n - 1) : n ** n - n + 1 }
+nuts = -> n { n.even? ? (n - 1) * (n ** n - 1) : n ** n - n + 1 }
 (1..30).each { |s| puts "#{s}: #{nuts[s]}" }
