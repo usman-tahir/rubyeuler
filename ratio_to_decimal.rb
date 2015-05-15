ba8a555bd131ae540ce63ef026979ed49cbdca91
Kelly Innes <kinnes@gmail.com>
2015-05-15 18:36:12 -0400

    convert ratio to decimal with a specific number of decimal places

---
+++ ratio_to_decimal.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# http://programmingpraxis.com/2015/05/15/convert-ratio-to-decimal/
+
+def ratio_to_decimal(numerator,denominator,digits)
+  val = numerator.divmod(denominator)
+  int_part = val[0]; remainder = val[1]
+  decimal_part = []
+  until remainder == 0 || decimal_part.count == digits
+    remainder = (remainder.to_s + "0").to_i
+    val = remainder.divmod(denominator)
+    decimal_part << val[0]; remainder = val[1]
+  end
+  "#{int_part}.#{decimal_part.map {|e| e.to_s}.join.to_i}".to_f
+end
+
+p ratio_to_decimal(3227,557,30)
