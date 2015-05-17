36b9fe6cf4329ea03efa49a2bce4670ed61f4141
Kelly Innes <kinnes@gmail.com>
2015-05-17 09:53:23 -0400

    cleaning up again

--- neweuler124.rb
+++ neweuler124.rb
@@ -4,10 +4,10 @@ require 'prime'
 
 def find_radical(n)
   accumulator = 1
-  n.prime_division.each {|e| accumulator *= e[0]}
+  n.prime_division.each { |e| accumulator *= e[0] }
   accumulator
 end
 
 limit = 100_000; vals = {}; vals[1] = 1
 (1..limit).each { |n| vals[n] = find_radical(n) }
-p Hash[vals.sort_by { |k,v| [v, k]}].to_a[9999][0]
+p vals.sort_by { |k,v| [v, k] }[9999][0]
