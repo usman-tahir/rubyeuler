2e190bf39c0c8f3d0c249c2aca8785d14c0eea23
Kelly Innes <kinnes@gmail.com>
2015-05-17 09:50:52 -0400

    solved problem 124

---
+++ neweuler124.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# https://projecteuler.net/problem=124
+require 'prime'
+
+def find_radical(n)
+  accumulator = 1
+  n.prime_division.each {|e| accumulator *= e[0]}
+  accumulator
+end
+
+limit = 100_000; vals = {}; vals[1] = 1
+(1..limit).each { |n| vals[n] = find_radical(n) }
+
+p Hash[vals.sort_by { |k,v| [1 * v, 1 * k]}].to_a[9999][0]
