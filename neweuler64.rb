a652fe07ff4d03cfdcac70a86c1904e5639a3725
Kelly Innes <kinnes@gmail.com>
2015-05-19 00:05:23 -0400

    solved problem 64

--- neweuler64.rb
+++ neweuler64.rb
@@ -2,18 +2,20 @@
 # https://projecteuler.net/problem=64
 # http://oeis.org/A003285
 
-limit = 13
+limit = 10_000
 
 def period_of_square_root(n)
-  period, e = [], Math.sqrt(n).floor
-  period << e
-  e = (1 / (Math.sqrt(n) - e))
-  period << e.floor
-  (e = (1 / (e - e.floor)); period << e.floor) until period[-1] == period[0] * 2
-  return period
+  a = Math.sqrt(n)
+  return 0 if a ** a == n
+  period = [a.floor]; d = 1; m = 0; a = a.to_i 
+  loop do
+    m = d*a - m
+    d = (n - m**2) / d
+    a = (Math.sqrt(n).to_i + m) / d
+    period << a
+    return period.count-1 if period[-1] == period[0] * 2
+  end
 end
 
-#values = (2..limit).map {|n| Math.sqrt(n) % 1 == 0 ? nil : n}.compact
-#p values.map! {|n| period_of_square_root(n).even? ? 1 : 0 }.inject(:+)
-
-p period_of_square_root(35)
+values = (2..limit).map {|n| Math.sqrt(n) % 1 == 0 ? nil : n}.compact
+p values.map! {|n| period_of_square_root(n).odd? ? 1 : 0 }.inject(:+)
