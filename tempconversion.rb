71da4ac36f6412f1bc053d2439359981ddc50e64
Kelly Innes <kinnes@gmail.com>
2015-05-25 16:14:51 -0400

    refactoring

--- tempconversion.rb
+++ tempconversion.rb
@@ -1,22 +1,20 @@
+#!/usr/bin/env ruby
 # http://rosettacode.org/wiki/Temperature_conversion
-start_time = Time.now
-temperature = 21.0
 
 def kelvin_to_c(temp)
-  celsius = temp - 273.15
+  (temp - 273.15).round(2)
 end
 
 def kelvin_to_f(temp)
-  fahrenheit = (kelvin_to_c(temp) * 1.800) + 32.00
+  ((kelvin_to_c(temp) * 1.800) + 32.00).round(2)
 end
 
 def kelvin_to_rankine(temp)
-  rankine = (kelvin_to_f(temp) - 32) + 491.67
+  ((kelvin_to_f(temp) - 32) + 491.67).round(2)
 end
 
-puts temperature
-puts kelvin_to_c(temperature).round(2)
-puts kelvin_to_f(temperature).round(2)
-puts kelvin_to_rankine(temperature).round(2)
-puts ((Time.now-start_time).to_f).to_s + "s"
-
+temperature = 21.0
+p "kelvin: #{temperature}"
+p "celsius: #{kelvin_to_c(temperature)}"
+p "fahrenheit: #{kelvin_to_f(temperature)}"
+p "rankine: #{kelvin_to_rankine(temperature)}"
