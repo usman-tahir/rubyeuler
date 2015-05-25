46c7e0589f9a4b19ada5388f7b1332b8709ecae6
Kelly Innes <kinnes@gmail.com>
2015-05-25 16:28:24 -0400

    refactoring

--- nonsquares.rb
+++ nonsquares.rb
@@ -1,28 +1,21 @@
+#!/usr/bin/env ruby
 # http://rosettacode.org/wiki/Sequence_of_non-squares
-# Print out the values for n in the range 1 to 22
-# Show that no squares occur for n less than one million 
-start_time = Time.now
 
-def function_of_non_squares(number)
+def nonsquare(number)
   (number + ((0.5) + Math.sqrt(number))).floor
 end  	
 
 def show_sequence_range(start_number, end_number)
-  sequence_array =[]
-  (start_number..end_number).each do |number|
-    sequence_array << function_of_non_squares(number)
-  end
-  sequence_array
+  seq = {}
+  (start_number..end_number).each { |n| seq[n] = nonsquare(n) }
+  return seq
 end
 
-def test_up_to_one_million
-  test_flag = false
-  (1..1_000_000).each do |number|
-    test_flag = true if Math.sqrt(function_of_non_squares(number)) % 1 == 0
-  end
-  test_flag # returns false if no n < 1_000_000 is a square root for f(n)
+def test_below(limit)
+  list = (1...limit).to_a.map { |i| nonsquare(i) }
+  list.each { |n| return false if (Math.sqrt(n) % 1 == 0) }
+  return true
 end		
 
-puts show_sequence_range(1, 22).inspect  	
-puts test_up_to_one_million
-puts ((Time.now - start_time).to_f).to_s + "s"
+p show_sequence_range(1, 22)	
+p test_below(1_000_000)
