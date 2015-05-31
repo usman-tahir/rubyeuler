21b8d91ba9188817d778ddd67fa0fe25473884fb
Kelly Innes <kinnes@gmail.com>
2015-05-31 18:57:08 -0400

    turned fizzbuzz function into macro

--- def_fizzbuzz.m
+++ def_fizzbuzz.m
@@ -3,12 +3,12 @@
 #define b @"buzz"
 #define fb @"fizzbuzz"
 #define n(i) ([NSString stringWithFormat:@"%d",i])
+#define fizzbuzz(i) (i % 15 == 0 ? fb : i % 5 == 0 ? b : i % 3 == 0 ? f : n(i))
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         for (int i = 1; i <= 100; i++) {
-            NSString *s = (i % 15 == 0 ? fb : i % 5 == 0 ? b : i % 3 == 0 ? f : n(i));
-            NSLog(@"%@", s);
+            NSLog(@"%@", fizzbuzz(i));
         }
     }
     return 0;
