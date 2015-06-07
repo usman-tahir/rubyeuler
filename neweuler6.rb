093d888a438bca7e0bed1d0486745dbcf67f2242
kellyi <kinnes@gmail.com>
2015-06-06 21:52:54 -0400

    refactoring

--- neweuler6.rb
+++ neweuler6.rb
@@ -1,23 +1,4 @@
+#!/usr/bin/env ruby
 # http://projecteuler.net/problem=6
 
-def sum_of_squares_of_first(number)
-  sum_of_squares = 0
-  (1..number).each do |num|
-    sum_of_squares += num ** 2
-  end
-  sum_of_squares	
-end
-
-def square_of_sums_of_first(number)
-  sum_accumulator = 0
-  (1..number).each do |num|
-    sum_accumulator += num
-  end
-  square_of_sums = sum_accumulator ** 2
-end		
-
-def squares_minus_sums(number)
-  square_of_sums_of_first(number) - sum_of_squares_of_first(number)
-end
-
-puts squares_minus_sums(100)
+p ((1..100).to_a.inject(:+) ** 2) - ((1..100).map {|e| e ** 2}.inject(:+))
