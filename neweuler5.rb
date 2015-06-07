349860fea5ec7072bdcfdf6a9abd96cf4863be4e
kellyi <kinnes@gmail.com>
2015-06-06 21:47:41 -0400

    refactoring

--- neweuler5.rb
+++ neweuler5.rb
@@ -1,9 +1,4 @@
 # http://projecteuler.net/problem=5
 
-def even_div(n)
-  (11..20).each {|d| return false if n % d != 0}; true
-end
-
-step_value = 9699690; smallest_n = 9699690
-(smallest_n += step_value) until even_div(smallest_n)
-p smallest_n
+even_div = -> n { (11..20).each {|d| return false if n % d != 0}; true }
+step = 9699690; n = 9699690; (n += step) until even_div[n]; p n
