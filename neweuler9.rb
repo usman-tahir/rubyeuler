4592ca94536c2c30b11c5b36c045063efe4a815d
kellyi <kinnes@gmail.com>
2015-06-07 20:39:37 -0400

    refactoring

--- neweuler9.rb
+++ neweuler9.rb
@@ -1,15 +1,11 @@
 # http://projecteuler.net/problem=9
 
 def find_special_pythagorean_triplet
-  (1..500).each do |x|
-    (1..500).each do |y|
-      (1..500).each do |z|
-        if (x**2) + (y**2) == (z**2) && x + y + z == 1000
-          return x * y * z
-        end
-      end
-    end      
-  end
+  (1..500).each {|x| (1..500).each {|y| (1..500).each {|z| return x * y * z if pyth_trip(x,y,z)}}}
+end
+
+def pyth_trip(x,y,z,n=1000)
+  (x**2) + (y**2) == (z**2) && x + y + z == n
 end
 
 puts find_special_pythagorean_triplet        
