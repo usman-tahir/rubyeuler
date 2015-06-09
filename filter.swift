fcf869c7feeb16dbbc58527afbd17baef28816f3
Kelly Innes <kinnes@gmail.com>
2015-06-08 22:13:03 -0400

    added helper function

--- filter.swift
+++ filter.swift
@@ -1,5 +1,9 @@
 // http://rosettacode.org/wiki/Filter
 
-let result = [1,2,3,4,5,6,7,8,9,10].filter( {$0 % 2 == 0})
+func even(n:Int) -> Bool {
+  return n % 2 == 0
+}
+
+let result = [1,2,3,4,5,6,7,8,9,10].filter( {even($0)} )
 print("\(result)\n")
 
