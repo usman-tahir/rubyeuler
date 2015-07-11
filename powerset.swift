dc3dc55cb2dffca5f31d964f3b9b6bc4621b6123
Kelly Innes <kinnes@gmail.com>
2015-07-10 21:13:52 -0400

    ignore duplicate elements

--- powerset.swift
+++ powerset.swift
@@ -3,24 +3,20 @@
 import Foundation
 
 func powerSet(list: [Int]) -> [[Int]]? {
-  var result = [list]
+  var result = [[],list]
   for element in list {
     let tempList = list.filter( { $0 != element } )
     result.append(tempList)
     for tempElement in tempList {
       let newTempList = tempList.filter( { $0 != tempElement } )
-      result.append(newTempList)
+      if !contains(result as Array,newTempList) {
+        result.append(newTempList)
+      }
     }
   }
-  var uniqueResult = [[]]
-  for element in result {
-    if !contains(uniqueResult,element) {
-      uniqueResult.append(element)
-    }
-  }
-  return uniqueResult as? [[Int]]
+  return result
 }
 
-var test = [1,2,3]
-var result = powerSet(test)
+let test = [1,2,3]
+let result = powerSet(test)
 println(result!)
