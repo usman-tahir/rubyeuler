505eac9cc147b0380daa9cc57a17e0f2021950a8
Kelly Innes <kinnes@gmail.com>
2015-07-18 00:08:14 -0400

    chore: Cut superfluous comments etc
    Trying out Udacity's commit message style. This part includes more
    detailed explanatory text. Here are some bullet points:
    
    - One
    - Two
    - Three
    
    Issue tracker references would go here, at the end.

--- praxis1.rb
+++ praxis1.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 # http://programmingpraxis.com/2014/02/18/two-interview-questions/
 
-# question 1
-# given n, find smallest three digit number such that the product of its digits is n
-
 def find_smallest_three_digit_product(n,t=100)
   digit_product = -> i { i.to_s.split(//).map { |e| e.to_i }.inject(:*) }
   t += 1 until digit_product[t] == n
@@ -12,7 +9,6 @@ end
 
 p find_smallest_three_digit_product(100)
 
-# question 2
 def compare_arrays(a,b)
   b.sort - a.sort
 end
