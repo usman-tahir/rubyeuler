68679a34f44b98bb5d742c682360ddbb54f72cc4
kellyi <kinnes@gmail.com>
2015-07-17 20:52:34 -0400

    convert word to pig latin in swift

---
+++ pig_latin.swift
@@ -0,0 +1,39 @@
+// http://programmingpraxis.com/2009/06/02/pig-latin/
+
+func initialVowel(w: String) -> String {
+  return w + "-way"
+}
+
+func initialConsonant(w: String, vowels: [String]) -> String {
+  var suffix = ""
+  var index = 0
+  while !contains(vowels,w[index]) {
+    suffix += w[index]
+    index++
+  }
+  var newString = ""
+  while index < count(w) {
+    newString += w[index]
+    index++
+  }
+  return "\(newString)-\(suffix)ay" 
+}
+
+func pigLatinize(w: String) -> String {
+  let vowels = ["a","e","i","o","u"]
+  if contains(vowels,w[0]) {
+    return initialVowel(w)
+  } else {
+    return initialConsonant(w,vowels)
+  }
+}
+
+extension String {
+  subscript (i: Int) -> String {
+    return String(Array(self)[i])
+  }
+}
+
+println(pigLatinize("abracadabra"))
+println(pigLatinize("boomshakalaka"))
+println(pigLatinize("stream"))
