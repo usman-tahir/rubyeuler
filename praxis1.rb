12d762e0d6501ba2b8b1aaa61e07630b9478a45c
kellyi <kinnes@gmail.com>
2015-07-17 20:22:36 -0400

    refactor

--- praxis1.rb
+++ praxis1.rb
@@ -1,22 +1,22 @@
+#!/usr/bin/env ruby
 # http://programmingpraxis.com/2014/02/18/two-interview-questions/
+
 # question 1
-runtime = Time.now
+# given n, find smallest three digit number such that the product of its digits is n
 
-store = Array.new
+def find_smallest_three_digit_product(n,t=100)
+  digit_product = -> i { i.to_s.split(//).map { |e| e.to_i }.inject(:*) }
+  t += 1 until digit_product[t] == n
+  return t
+end
 
-# finds smallest three digit number
-def productize(num)
-	for a in 100..999
-	store = a.to_s.split('').map(&:to_i)
-		if store.inject(:*) == num
-			puts a
-			break
-		end
-	end
-end			
+p find_smallest_three_digit_product(100)
 
-print "Please enter a number to test: "
-input = gets.chomp.to_i
+# question 2
+def compare_arrays(a,b)
+  b.sort - a.sort
+end
 
-productize(input)
-puts ((Time.now - runtime).to_f).to_s + " s"
+one = [4,7,9,6,1,3,8] 
+two = [3,4,1,8,2,9,5,7,6]
+p compare_arrays(one,two)
