1553867bccb78d2d454e919d7b722948a4a50f5e
kellyi <kinnes@gmail.com>
2015-07-30 12:11:21 -0400

    refactor: Rewrite split and sum as one-liners

--- luhn.swift
+++ luhn.swift
@@ -1,10 +1,8 @@
+#!/usr/bin/env swift
 // http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers
 
 func checkSum(n:Int) -> Bool {
-  var checkArray:[Int] = []
-  // reverse the digits
-  checkArray = split(n,checkArray)
-  // sum the odd digits
+  var checkArray = split(n)
   var s1:Int = 0
   var s2:Int = 0
   for var i = 0; i < checkArray.count; i++ {
@@ -17,38 +15,23 @@ func checkSum(n:Int) -> Bool {
       let evenDigits = checkArray[i] * 2
       if evenDigits < 10 {
         s2 += evenDigits
-      } else if evenDigits >= 10 {
-        var evenDigitsArray:[Int] = []
-        s2 = s2 + sum(split(evenDigits,evenDigitsArray))
+      } else {
+        s2 = s2 + sum(split(evenDigits))
       }
     }
   }
   return (s1 + s2) % 10 == 0
 }
 
-func split(n:Int,array:[Int]) -> [Int] {
-  var mutableArray = array
-  if n < 10 {
-    mutableArray.append(n)
-    return mutableArray 
-  } else {
-    mutableArray.append(n%10)
-    return split(n/10,mutableArray)
-  }
+func split(n:Int) -> [Int] {
+  return reverse(Array(String(n))).map { "\($0)".toInt()! }
 }
 
 func sum(array:[Int]) -> Int {
-  var arraySum = 0
-  for var i = 0; i < array.count; i++ {
-    arraySum += array[i]
-  }
-  return arraySum
+  return array.reduce(0) { $0 + $1 }
 }
 
-let numbersToCheck:[Int] = [49927398716, 49927398717, 
-                            1234567812345678, 1234567812345670]
-
-for number in numbersToCheck {
-  let value:Bool = checkSum(number)
-  println("\(number): \(value)")
+for number in [49927398716, 49927398717, 
+                            1234567812345678, 1234567812345670] {
+  println("\(number): \(checkSum(number))")
 }
