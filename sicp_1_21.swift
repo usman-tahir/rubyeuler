d698af2423221ae36339add1a737e702dcc2acbe
kellyi <kinnes@gmail.com>
2015-08-05 21:16:56 -0400

    refactor: Solve SICP exercise 1.21 in Swift

--- sicp_1_21.swift
+++ sicp_1_21.swift
@@ -1,35 +1,26 @@
-// find smallest divisor of an int
+#!/usr/bin/env swift
+// sicp exercise 1.21
 
-func square(x:Int) -> Int {
+func square(x: Int) -> Int {
   return x * x
 }
 
-func smallestDivisor(x:Int) -> Int {
+func smallestDivisor(x: Int) -> Int {
   return findDivisor(x,2)
 }
 
-func findDivisor(n:Int,testDivisor:Int) -> Int {
-  if (square(testDivisor) > n) {
-    return n
-  } else if divides(testDivisor,n) {
-    return testDivisor
-  } else {
-    return findDivisor(n,(testDivisor+1))
-  }
+func findDivisor(n: Int, t: Int) -> Int {
+  return square(t) > n ? n : divides(t,n) ? t : findDivisor(n,(t+1))
 }
 
-func divides(a:Int,b:Int) -> Bool {
+func divides(a: Int, b: Int) -> Bool {
   return b % a == 0
 }
 
-func prime(n:Int) -> Bool {
+func prime(n: Int) -> Bool {
   return smallestDivisor(n) == n
 }
 
-let a = smallestDivisor(199)
-let b = smallestDivisor(1999)
-let c = smallestDivisor(19999)
-
-println("199's smallest divisor: \(a)")
-println("1999's smallest divisor: \(b)")
-println("19999's smallest divisor: \(c)")
+println(smallestDivisor(199))
+println(smallestDivisor(1999))
+println(smallestDivisor(19999))
