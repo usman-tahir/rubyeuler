d364d2756c20d90c54926290bb089a7f1c42166b
kellyi <kinnes@gmail.com>
2015-08-05 20:39:23 -0400

    refactor: Solve SICP exercise 1.16 in Swift

--- sicp_1_16.swift
+++ sicp_1_16.swift
@@ -1,25 +1,20 @@
-// sicp 1.16: fast recursive exponentiation
+#!/usr/bin/env swift
+// sicp exercise 1.16
 
-func square(x:Int) -> Int {
-  return (x * x)
+func square(x: Int) -> Int {
+  return x * x
 }
 
-func even(x:Int) -> Bool {
+func even(x: Int) -> Bool {
   return x % 2 == 0
 }
 
-func fastExpt(b:Int,n:Int) -> Int {
+func fastExpt(b: Int, n: Int) -> Int {
   return fastExptIter(b,n,1)
 }
 
-func fastExptIter(b:Int,counter:Int,product:Int) -> Int {
-  if (counter == 0) {
-    return product
-  } else if (even(counter)) {
-    return fastExptIter(square(b),(counter/2),product)
-  } else {
-    return fastExptIter(b,(counter-1),(b*product))
-  }
+func fastExptIter(b: Int, counter: Int, prod: Int) -> Int {
+  return counter == 0 ? prod : even(counter) ? fastExptIter(square(b),(counter/2),prod) : fastExptIter(b,(counter-1),(b*prod))
 }
 
 println(fastExpt(3,2))
