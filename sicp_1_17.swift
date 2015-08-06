027526a5513e6d0f6a832a5bf45006e1ac96faa9
kellyi <kinnes@gmail.com>
2015-08-05 20:42:45 -0400

    refactor: Solve SICP exercise 1.17 in Swift

--- sicp_1_17.swift
+++ sicp_1_17.swift
@@ -1,18 +1,19 @@
-// http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-11.html#%_sec_1.2
+#!/usr/bin/env swift
+// sicp exercise 1.17
 
-func fastMultiplication(x:Int,y:Int) -> Int {
+func fastMultiplication(x: Int, y: Int) -> Int {
   return multiIter(x,y,0)
 }
 
-func multiIter(x:Int,y:Int,counter:Int) -> Int {
-  if (y == 0) {
-    return counter
-  } else if (y % 2 == 0) {
-    return multiIter((x+x),(y/2),(counter))
-  } else {
-    return multiIter(x,(y-1),(counter+x))
-  }
+func even(x: Int) -> Bool {
+  return x % 2 == 0
+}
+
+func multiIter(x: Int, y: Int, counter: Int) -> Int {
+  return y == 0 ? counter : even(y) ? multiIter((x+x),(y/2),(counter)) : multiIter(x,(y-1),(counter+x))
 }
 
 println(fastMultiplication(5,5))
 println(fastMultiplication(7,9))
+println(fastMultiplication(10,10))
+println(fastMultiplication(12,12))
