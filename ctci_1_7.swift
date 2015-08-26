4a08e7d878f7678efce1d2ffd46e07fe78788a0c
Kelly Innes <kinnes@gmail.com>
2015-08-26 19:01:47 -0400

    refactor: Cut some lines

--- ctci_1_7.swift
+++ ctci_1_7.swift
@@ -2,26 +2,23 @@
 // cracking the coding interview q 1.7
 
 func zeroMatrixRowAndColumn(matrix: [[Int]]) -> [[Int]] {
+  var result = matrix
   var zeroPoints: [[Int]] = [[],[]]
   for rowIndex in 0..<matrix.count {
-    let currentRow = matrix[rowIndex]
-    if contains(currentRow,0) {
+    if contains(matrix[rowIndex],0) {
       zeroPoints[0].append(rowIndex)
     }
-    for colIndex in 0..<currentRow.count {
-      if currentRow[colIndex] == 0 {
+    for colIndex in 0..<matrix[rowIndex].count {
+      if matrix[rowIndex][colIndex] == 0 {
         zeroPoints[1].append(colIndex)
       }
     }
   }
   zeroPoints = [Array(Set(zeroPoints[0])),Array(Set(zeroPoints[1]))]
-  var result = matrix
   for rowIndex in 0..<matrix.count {
     for colIndex in 0..<matrix[rowIndex].count {
       if contains(zeroPoints[0],rowIndex) || contains(zeroPoints[1],colIndex) {
         result[rowIndex][colIndex] = 0
-      } else {
-        result[rowIndex][colIndex] = matrix[rowIndex][colIndex]
       }
     }
   }
