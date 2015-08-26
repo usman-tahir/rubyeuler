000e85fc36fe01f7a2254cc33aff0c3077ca39ed
Kelly Innes <kinnes@gmail.com>
2015-08-25 23:02:35 -0400

    fix: handle palindrome array with even # elements

--- array_palindrome.swift
+++ array_palindrome.swift
@@ -4,7 +4,7 @@
 func arrayPalindrome(n: [Int]) -> Bool {
   var leftIndex = 0
   var rightIndex = n.count-1
-  while rightIndex != leftIndex {
+  while rightIndex != leftIndex && rightIndex > leftIndex {
     if n[leftIndex] != n[rightIndex] {
       return false
     } else {
@@ -15,7 +15,7 @@ func arrayPalindrome(n: [Int]) -> Bool {
   return true
 }
 
-let palin = [1,2,3,4,3,2,1]
+let palin = [1,2,3,4,4,3,2,1]
 let notPalin = [1,2,3,4,5,1,2,3,4,5]
 
 println(arrayPalindrome(palin))
