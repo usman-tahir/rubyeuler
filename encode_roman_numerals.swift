cf9f6f1659680ff7c3d3aa380cf8d79f29fddf2f
Kelly Innes <kinnes@gmail.com>
2015-08-29 22:33:37 -0400

    chore: Use Int type instead of Double in Switch

--- encode_roman_numerals.swift
+++ encode_roman_numerals.swift
@@ -9,75 +9,75 @@ func split(n: Int) -> [Int] {
 
 func romanNumeral(n: Int) -> String {
   var romanNumeralString = ""
-  var splitN = split(n).map { Double($0) }
+  var splitN = split(n)
   for i in 0..<splitN.count {
-    splitN[i] = splitN[i] * pow(10,Double(i))
+    splitN[i] = splitN[i] * Int(pow(10,Double(i)))
   }
   for var i = splitN.count - 1; i >= 0; i-- {
     switch splitN[i] {
-    case 5000.0:
+    case 5000:
       romanNumeralString += "MMMMM"
-    case 4000.0:
+    case 4000:
       romanNumeralString += "MMMM"
-    case 3000.0:
+    case 3000:
       romanNumeralString += "MMM"    
-    case 2000.0:
+    case 2000:
       romanNumeralString += "MM"
-    case 1000.0:
+    case 1000:
       romanNumeralString += "M"
-    case 900.0:
+    case 900:
       romanNumeralString += "CM"
-    case 800.0:
+    case 800:
       romanNumeralString += "DCCC"
-    case 700.0:
+    case 700:
       romanNumeralString += "DCC"
-    case 600.0:
+    case 600:
       romanNumeralString += "DC"
-    case 500.0:
+    case 500:
       romanNumeralString += "D"
-    case 400.0:
+    case 400:
       romanNumeralString += "CD"
-    case 300.0:
+    case 300:
       romanNumeralString += "CCC"
-    case 200.0:
+    case 200:
       romanNumeralString += "CC"
-    case 100.0:
+    case 100:
       romanNumeralString += "C"
-    case 90.0:
+    case 90:
       romanNumeralString += "XC"
-    case 80.0:
+    case 80:
       romanNumeralString += "LXXX"
-    case 70.0:
+    case 70:
       romanNumeralString += "LXX"
-    case 60.0:
+    case 60:
       romanNumeralString += "LX"
-    case 50.0:
+    case 50:
       romanNumeralString += "L"
-    case 40.0:
+    case 40:
       romanNumeralString += "XL"
-    case 30.0:
+    case 30:
       romanNumeralString += "XXX"
-    case 20.0:
+    case 20:
       romanNumeralString += "XX"
-    case 10.0:
+    case 10:
       romanNumeralString += "X"
-    case 9.0:
+    case 9:
       romanNumeralString += "IX"
-    case 8.0:
+    case 8:
       romanNumeralString += "VIII"
-    case 7.0:
+    case 7:
       romanNumeralString += "VII"
-    case 6.0:
+    case 6:
       romanNumeralString += "VI"
-    case 5.0:
+    case 5:
       romanNumeralString += "V"
-    case 4.0:
+    case 4:
       romanNumeralString += "IV"
-    case 3.0:
+    case 3:
       romanNumeralString += "III"
-    case 2.0:
+    case 2:
       romanNumeralString += "II"
-    case 1.0:
+    case 1:
       romanNumeralString += "I"
     default:
       romanNumeralString += ""
