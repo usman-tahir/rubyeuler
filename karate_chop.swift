44fc640145d14cf27e4a96af3bc26bbed6345976
kellyi <kinnes@gmail.com>
2015-10-10 20:47:38 -0400

    feat: redo binary search kata in Swift

--- karate_chop.swift
+++ karate_chop.swift
@@ -1,49 +1,22 @@
+#!/usr/bin/env swift
 // http://codekata.com/kata/kata02-karate-chop/
 
-func bOne(array: [Int], element: Int, low: Int, high: Int) -> Int? {
-  if high < low { return nil }
-  var m = (low + high) / 2
-  if array[m] == element { return m }
-  return array[m] > element ? bOne(array,element,low,m-1) : bOne(array,element,m+1,high) 
-}
-
-func bTwo(array: [Int], element: Int) -> Int? {
-  var mid = array.count / 2
-  while array[mid] != element {
-    if array[mid] > element {
-      mid--
-    } else if array[mid] < element {
-      mid++
+func binarySearchOne(list: [Int], elementToFind: Int, high: Int, low: Int = 0) -> Int? {
+  if low >= high {
+    return nil
+  } else {
+    let mid = (low + high) / 2
+    if list[mid] > elementToFind {
+      return binarySearchOne(list, elementToFind: elementToFind, high: mid-1, low: low)
+    } else if list[mid] < elementToFind {
+      return binarySearchOne(list, elementToFind: elementToFind, high: high, low: mid+1)
+    } else {
+      return mid
     }
-    if array[mid+1] > element && array[mid] < element {
-      return nil
-    } else if array[mid] > element && array[mid-1] < element {
-      return nil
-    } 
   }
-  return mid
 }
 
-func bThree(array: [Int], element: Int) -> Int? {
-  var low = 0
-  var high = array.count
-  var mid = high/2
-  while array[mid] != element {
-    if element < array[mid] {
-      high = mid
-      mid = (low + mid) / 2
-    } else if element > array[mid] {
-      low = mid
-      mid = (mid + high) / 2
-    }
-    if array[mid+1] > element && array[mid] < element { return nil }
-    if array[mid] > element && array[mid-1] < element { return nil }
-  }
-  return mid
-}
+let list = [0,1,4,5,6,7,8,9,12,26,45,67,78,90,98,123,211,234,456,769,865,2345,3215,14345,24324]
+let values = [0,42,45,24324,99999]
 
-let test = [0,1,4,5,6,7,8,9,12,26,45,67,78,90,98,123,211,234,456,769,865,2345,3215,14345,24324]
-
-println(bOne(test,98,0,test.count))
-println(bTwo(test,98))
-println(bThree(test,98))
+for v in values { print(binarySearchOne(list, elementToFind: v, high: list.count)) }
