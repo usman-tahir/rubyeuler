e0103a8abb508852295292457ff1d5646fde532f
kellyi <kinnes@gmail.com>
2015-10-25 21:54:58 -0400

    feat: find 3 elements which sum to zero
    - clean up code
    - use swift 2 syntax
    - only return unique sets of three elements

--- 3sum.swift
+++ 3sum.swift
@@ -1,19 +1,19 @@
+#!/usr/bin/env swift
 // http://programmingpraxis.com/2013/06/18/3sum/
 
-func findZeroSumTerms(array: [Int]) -> [Int] {
-  var solutions: [Int] = []
-  for var i = 0; i < array.count; i++ {
-    for var j = i; j < array.count; j++ {
-      var testVal = -(array[i] + array[j])
-      if contains(array,testVal) {
-        solutions.append(array[i]) 
-        solutions.append(array[j])
-        solutions.append(testVal)
+func findZeroSumTerms(array: [Int]) -> [[Int]] {
+  var solutions:[String:[Int]] = [:]
+  for i in 0..<array.count {
+    for j in i..<array.count {
+      let testVal = -(array[i] + array[j])
+      if array.contains(testVal) {
+        let threeToAppend = [array[i], array[j], testVal].sort { $0 < $1 }
+        solutions["\(threeToAppend)"] = threeToAppend
       }
     }
   }
-  return solutions
+  return Array(solutions.values)
 }
 
-var testArray = [8, -25, 4, 10, -10, -7, 2, -3] 
-println(findZeroSumTerms(testArray))
+let testArray = [8, -25, 4, 10, -10, -7, 2, -3] 
+print(findZeroSumTerms(testArray))
