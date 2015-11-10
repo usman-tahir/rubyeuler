f9208cc90fe4c34a6e53d873becc5a9f20dd5733
Kelly Innes <kinnes@gmail.com>
2015-11-09 23:32:34 -0500

    refactor: messing around with a selection sort

--- selection_sort.swift
+++ selection_sort.swift
@@ -1,21 +1,24 @@
+#!/usr/bin/env swift
 // http://rosettacode.org/wiki/Sorting_algorithms/Selection_sort
+
+func mySwap(a: Int, b: Int) -> (Int, Int) {
+  return (b, a)
+}
 
 func selectionSort(arrayToSort:[Int]) -> [Int] {
   var mutableArray = arrayToSort
-  for var i = 0; i < mutableArray.count; i++ {
-    var min = i
-    while min < mutableArray.count {
-      if mutableArray[min] < mutableArray[i] {
-        swap(&mutableArray[i], &mutableArray[min])
+  for i in 0..<mutableArray.count {
+    for j in i..<mutableArray.count {
+      if mutableArray[j] < mutableArray[i] {
+        (mutableArray[j], mutableArray[i]) = mySwap(mutableArray[j], b: mutableArray[i])
       }
-      min += 1
     }
   }
   return mutableArray
 }
 
-let testArray1 = [26, 45, 4, 98, 769, 234, 24324, 78, 865, 0, 6, 123, 7, 14345, 5, 12, 211, 1, 3215, 67, 8, 90, 9, 2345, 456]
-let testArray2 = [7,3,1,5,10,0,4]
+let t1 = [26, 45, 4, 98, 769, 234, 24324, 78, 865, 0, 6, 123, 7, 14345, 5, 12, 211, 1, 3215, 67, 8, 90, 9, 2345, 456]
+let t2 = [7,3,1,5,10,0,4]
 
-println(selectionSort(testArray2))
-println(selectionSort(testArray1))
+print(selectionSort(t1))
+print(selectionSort(t2))
