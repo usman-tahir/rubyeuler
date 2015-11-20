11641b78ff68088824698267d92665bd843e4362
Kelly Innes <kinnes@gmail.com>
2015-11-19 22:47:39 -0500

    fix: bugfix

--- next_bigger_number.rb
+++ next_bigger_number.rb
@@ -2,9 +2,9 @@
 # http://www.codewars.com/kata/next-bigger-number-with-the-same-digits
 
 def next_bigger_number(n)
-  numbers = n.to_s.split("").to_a.permutation.to_a.map { |e| e.join.to_i }.sort
+  numbers = n.to_s.split("").to_a.permutation.to_a.map { |e| e.join.to_i }.sort.uniq
   numbers.index(n) + 1 == numbers.count ? -1 : numbers[numbers.index(n)+1]
 end
 
 [12,513,2017].each { |e| puts "#{e} => #{next_bigger_number(e)}" }
-[9,11,531].each { |e| puts "#{e} => #{next_bigger_number(e)}" }
+[9,111,531].each { |e| puts "#{e} => #{next_bigger_number(e)}" }
