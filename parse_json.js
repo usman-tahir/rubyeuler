4657a1dc110f59f19c34529a89411c0833d17674
kellyi <kinnes@gmail.com>
2015-11-26 16:55:07 -0500

    fix: error handling && output

--- parse_json.js
+++ parse_json.js
@@ -2,16 +2,18 @@
 "use strict";
 
 var request = require('request');
-var url = 'http://www3.septa.org/hackathon/RRSchedules/458';
-request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+var number = 458
+var url = 'http://www3.septa.org/hackathon/RRSchedules/' + number;
+request(url, number, function (error, response, body) {
+  if (body) {
     var septaJSON = JSON.parse(body);
+    console.log("Train " + number + " Schedule")
     for (var i in septaJSON) {
       var train = septaJSON[i].station
       var time = septaJSON[i].sched_tm
       console.log(train + " at " + time)
     }
   } else {
-    console.log(error + " : " + response.statusCode);
+    console.log(error);
   }
 });
