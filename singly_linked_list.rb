32f0b43689633f846c72256b92da69b69fb37e1c
kellyi <kinnes@gmail.com>
2015-12-11 20:10:32 -0500

    feat: more linked list ops in ruby

--- singly_linked_list.rb
+++ singly_linked_list.rb
@@ -17,10 +17,46 @@ class Link
     @subseq = next_link
   end
 
+  def append(link_to_append)
+    link_to_append.subseq = @subseq
+    @subseq = link_to_append
+  end
+
+  def delete_subseq
+    @subseq = @subseq.subseq
+  end
+
 end
 
-a = Link.new(1)
-b = Link.new(2)
+a = Link.new("link one")
+b = Link.new("link two")
 a.subseq = b
 p a.subseq.data
 p b.subseq
+c = Link.new("link three")
+a.append(c)
+p a.subseq.data
+p c.subseq.data
+a.delete_subseq
+p a.subseq.data
+
+def traverse_list(current_node)
+  p current_node.data
+  if current_node.subseq == nil  
+    return
+  else 
+    return traverse_list(current_node.subseq)
+  end
+end
+
+one = Link.new("1")
+two = Link.new("2")
+three = Link.new("3")
+four = Link.new("4")
+one.append(two)
+two.append(three)
+three.append(four)
+traverse_list(one)
+
+two.delete_subseq
+traverse_list(one)
