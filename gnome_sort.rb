efdb8f99eeae9781722168cbb728aaa6493f880a
kellyi <kinnes@gmail.com>
2015-12-15 22:26:57 -0500

    chore: cleaning up

--- gnome_sort.rb
+++ gnome_sort.rb
@@ -1,23 +1,19 @@
+#!/usr/bin/env ruby
 # http://rosettacode.org/wiki/Sorting_algorithms/Gnome_sort
 
-test_array = [100, 2, 56, 200, -52, 3, 99, 33, 177, -199] # from C++ version
-
-def gnome_sort(array)
-  i = 1
-  j = 2
-  while i < array.size do
-    if array[i-1] <= array[i]
-      i = j
-      j = j + 1
+def gnome_sort(list)
+  i, j = 1, 2
+  while i < list.size do
+    if list[i-1] <= list[i]
+      (i, j = j, j+1)
     else
-      array[i-1], array[i] = array[i], array[i-1] # swap
+      list[i-1], list[i] = list[i], list[i-1]
       i -= 1
-      if i == 0 then
-        i, j = j, j + 1
-      end
+      (i, j = j, j + 1) if i == 0
     end
   end
-  array
+  list
 end          
 
+test_array = [100, 2, 56, 200, -52, 3, 99, 33, 177, -199]
 p gnome_sort(test_array)
