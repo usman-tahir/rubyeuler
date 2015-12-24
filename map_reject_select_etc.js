d8c7fbc15421f26f1bdb68613eeb825c105d35f2
Kelly Innes <kinnes@gmail.com>
2015-12-23 21:29:20 -0500

    feat: add simple reduce method

--- map_reject_select_etc.js
+++ map_reject_select_etc.js
@@ -24,6 +24,18 @@ function _select(list,f) {
   return results
 }
 
+function _reduce(list,f,baseValue) {
+  var result = baseValue
+  if (list.length === 0) { return null }
+  if (list.length === 1) { return list[0] }
+  function eatList(list,f,v,i) {
+    if (i === undefined) { i = 0 }
+    if (i === list.length) { return v }
+    return eatList(list,f,f(v,list[i]),i+1)
+  }
+  return eatList(list,f,result)
+}
+
 var r = [1,2,3,4,5,6,7,8,9,10]
 var dbl = function (n) { return n * 2 }
 console.log(_map(r,dbl))
@@ -36,3 +48,9 @@ console.log(_select(r,isAString))
 
 var makeIntoString = function (n) { return n + "" }
 console.log(_map(r,makeIntoString))
+
+var _add = function (a,b) { return a + b }
+var _multiply = function(a,b) { return a * b }
+var l = [2,3,4]
+console.log(_reduce(l,_add,0))
+console.log(_reduce(l,_multiply,1))
