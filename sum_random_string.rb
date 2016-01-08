e9ef8cea855b628609e42bcdd8381d65dff065e8
kellyi <kinnes@gmail.com>
2016-01-07 23:02:50 -0500

    feat: revise into lambda exp

--- sum_random_string.rb
+++ sum_random_string.rb
@@ -1,12 +1,6 @@
 #!/usr/bin/env ruby
 # http://www.codewars.com/kata/sum-up-the-random-string
 
-def sum_random_string(s)
-  result = s.split(/[^0-9]/).map { |e| e == "" ? nil : e.to_i }.compact.inject(:+)
-  return result == nil ? 0 : result
-end
-
-str = "In 2015, I want to know how much does iPhone 6+ cost?"
-t = "hello world"
-p sum_random_string(str)
-p sum_random_string(t)
+sum_random_string = -> s { s.split(/[^0-9]/).map { |e| e == "" ? nil : e.to_i }.compact.inject(:+) || 0 }
+s, t = "In 2015, I want to know how much does iPhone 6+ cost?", "hello world"
+p sum_random_string[s]; p sum_random_string[t]
