061ae55d40554a364b221402f08bea67bfc70369
kellyi <kinnes@gmail.com>
2016-01-21 20:28:08 -0500

    refactor: kata in ruby

--- two_word_games.rb
+++ two_word_games.rb
@@ -1,38 +1,19 @@
+#!/usr/bin/env ruby
 # http://programmingpraxis.com/2012/10/09/two-word-games/
 
-# 1. find all words in dictionary with exactly 5 vowels in ascending order
 def five_ascending_vowels(word)
-  vowels = ["a","e","i","o","u"]
-  word_vowels = []
-  word.split(//).each do |char|
-    word_vowels << char if vowels.include?(char)
-  end
-  word_vowels == vowels  
+  v = ["a","e","i","o","u"]
+  v == word.split("").select { |c| v.include?(c) }
 end
 
-# 2. find six+ char long words with letters in ascending alphabetical order
-def ascending_alpha_order(word)
-  return false if word.length < 6
-  word.split(//) == word.split(//).sort
+def six_ascending_alpha_order(word)
+  return word.length < 6 ? false : word.split("") == word.split("").sort
 end
 
-def find_five_asc_vowel_words(file)
-  words = []
-  f = File.new(file)
-  f.each do |word|
-    words << word.chomp if five_ascending_vowels(word.chomp)
-  end
-  words  
+def find_words_for_test(dict,test)
+  File.new(dict).each { |w| puts w.chomp if test.call(w.chomp) }
 end
 
-def find_asc_alpha_order_words(file)
-  words = []
-  f = File.new(file)
-  f.each do |word|
-    words << word.chomp if ascending_alpha_order(word.chomp)
-  end
-  words  
-end
+find_words_for_test("unixdict.txt", method(:five_ascending_vowels))
+find_words_for_test("unixdict.txt", method(:six_ascending_alpha_order))
 
-p find_five_asc_vowel_words("unixdict.txt")
-p find_asc_alpha_order_words("unixdict.txt")
