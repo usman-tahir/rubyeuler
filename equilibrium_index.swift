0b2704846bb9a7a35b51e82c8de4703d78fb08ac
Kelly Innes <kinnes@gmail.com>
2016-01-27 20:23:28 -0500

    feat: find equilibrium index in swift

---
+++ equilibrium_index.swift
@@ -0,0 +1,17 @@
+#!/usr/bin/env swift
+// http://rosettacode.org/wiki/Equilibrium_index
+
+func equilibriumIndex(list: [Int]) -> [Int] {
+  var eqIndices = [Int]()
+  for i in 1..<list.count {
+    let firstPart = Array(list[0..<i+1]).reduce(0, combine: +)
+    let secondPart = Array(list[i..<list.count]).reduce(0, combine: +)
+    if firstPart == secondPart {
+      eqIndices.append(i)
+    }
+  }
+  return eqIndices
+}
+
+let t = [-7,1,5,2,-4,3,0]
+print(equilibriumIndex(t))
