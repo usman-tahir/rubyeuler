5cf7f13336e0eab6cb839d1e48316d969cca476d
Kelly Innes <kinnes@gmail.com>
2016-02-10 00:13:10 -0500

    chore: use map & reduce

--- py_euler6.py
+++ py_euler6.py
@@ -3,16 +3,10 @@
 def square(n):
   return n * n
 
-def sum_list(list):
-  def _sum_list(list,index,accumulator):
-    if index == len(list):
-      return accumulator
-    else:
-      return _sum_list(list,index+1,accumulator+list[index])
-  return _sum_list(list,0,0)
+_sum = lambda x,y: x + y
 
 def sum_squares_minus_square_sums(n):
-  return square(sum_list(list(range(1,n+1)))) - \
-    sum_list([square(i) for i in list(range(1,n+1))])
+  return square(reduce(_sum, list(range(1,n+1)))) - \
+    reduce(_sum, map(square,list(range(1,n+1))))
 
 print sum_squares_minus_square_sums(100)
