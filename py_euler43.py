55ce0cc0a8c39cfb5dd94d0aed7dffd2f3b7887a
Kelly Innes <kinnes@gmail.com>
2016-03-09 22:20:13 -0500

    fix: change variable names

--- py_euler43.py
+++ py_euler43.py
@@ -2,37 +2,37 @@
 
 from itertools import permutations
 
-def join_int(list):
-    return int("".join([str(digit) for digit in list]))
+def join_int(array):
+    return int("".join([str(digit) for digit in array]))
 
-def div_by_two(list):
-    return join_int(list[1:4]) % 2 == 0
+def div_by_two(array):
+    return join_int(array[1:4]) % 2 == 0
 
-def div_by_three(list):
-    return join_int(list[2:5]) % 3 == 0
+def div_by_three(array):
+    return join_int(array[2:5]) % 3 == 0
 
-def div_by_five(list):
-    return join_int(list[3:6]) % 5 == 0
+def div_by_five(array):
+    return join_int(array[3:6]) % 5 == 0
 
-def div_by_seven(list):
-    return join_int(list[4:7]) % 7 == 0
+def div_by_seven(array):
+    return join_int(array[4:7]) % 7 == 0
 
-def div_by_eleven(list):
-    return join_int(list[5:8]) % 11 == 0
+def div_by_eleven(array):
+    return join_int(array[5:8]) % 11 == 0
 
-def div_by_thirteen(list):
-    return join_int(list[6:9]) % 13 == 0
+def div_by_thirteen(array):
+    return join_int(array[6:9]) % 13 == 0
 
-def div_by_seventeen(list):
-    return join_int(list[7:10]) % 17 == 0
+def div_by_seventeen(array):
+    return join_int(array[7:10]) % 17 == 0
 
-def div_by_all(list):
-    return div_by_two(list) and \
-           div_by_three(list) and \
-           div_by_five(list) and \
-           div_by_seven(list) and \
-           div_by_eleven(list) and \
-           div_by_thirteen(list) and \
-           div_by_seventeen(list)
+def div_by_all(array):
+    return div_by_two(array) and \
+           div_by_three(array) and \
+           div_by_five(array) and \
+           div_by_seven(array) and \
+           div_by_eleven(array) and \
+           div_by_thirteen(array) and \
+           div_by_seventeen(array)
 
 print sum([join_int(x) for x in list(permutations(xrange(0,10))) if div_by_all(x)])
