2f0eb968c0c56c2469f008c265f6cbb7e7972504
Kelly Innes <kinnes@gmail.com>
2016-03-19 23:31:25 -0400

    feat: update function

--- py_euler5.py
+++ py_euler5.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
 def evenly_divisible_by_one_through_twenty(n):
-    for d in xrange(11,21):
-        if n % d != 0:
-            return False
-    return True
+    return len([d for d in xrange(1,21) if n % d != 0]) == 0
 
 step = reduce(lambda a, b: a * b, [x for x in (2, 3, 5, 7, 11, 13, 17, 19)])
 counter = step
